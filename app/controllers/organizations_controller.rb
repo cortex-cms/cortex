43c33e0e9a7ccca11af92d45fa7d66b45acffaf1
Bennett Goble <Bennett.Goble@careerbuilder.com>
2013-11-27 14:50:29 -0500

    Cleaned up Users and Organizations controllers

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -1,64 +1,31 @@
 class OrganizationsController < ApplicationController
   before_action :set_organization, only: [:show, :edit, :update, :destroy]
+  repond_to :json
 
   # GET /organizations
-  # GET /organizations.json
   def index
     @organizations = Organization.all
   end
 
   # GET /organizations/1
-  # GET /organizations/1.json
   def show
   end
 
-  # GET /organizations/new
-  def new
-    @organization = Organization.new
-  end
-
-  # GET /organizations/1/edit
-  def edit
-  end
-
   # POST /organizations
-  # POST /organizations.json
   def create
     @organization = Organization.new(organization_params)
-
-    respond_to do |format|
-      if @organization.save
-        format.html { redirect_to @organization, notice: 'Organization was successfully created.' }
-        format.json { render action: 'show', status: :created, location: @organization }
-      else
-        format.html { render action: 'new' }
-        format.json { render json: @organization.errors, status: :unprocessable_entity }
-      end
-    end
+    repond_with @organization
   end
 
   # PATCH/PUT /organizations/1
-  # PATCH/PUT /organizations/1.json
   def update
-    respond_to do |format|
-      if @organization.update(organization_params)
-        format.html { redirect_to @organization, notice: 'Organization was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: 'edit' }
-        format.json { render json: @organization.errors, status: :unprocessable_entity }
-      end
-    end
+    respond_with @organization.update(organization_params)
   end
 
   # DELETE /organizations/1
-  # DELETE /organizations/1.json
   def destroy
     @organization.destroy
-    respond_to do |format|
-      format.html { redirect_to organizations_url }
-      format.json { head :no_content }
-    end
+    respond_with head :no_content
   end
 
   private
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,37 +2,36 @@ class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
   respond_to :json
 
+  # GET /users
   def index
     @users = User.all
     respond_with @users
   end
 
+  # GET /users/1
   def show
   end
 
+  # GET /users/me
   def me
     @user = @current_user
     render :show
   end
 
-  def new
-    respond_with User.new
-  end
-
-  def edit
-  end
-
+  # POST /users
   def create
     @user = User.new(user_params)
     @user.save
     respond_with @user
   end
 
+  # PATCH/PUT /users
   def update
     @user.update(user_params)
     respond_with @user
   end
-
+  
+  # DELETE /users/1
   def destroy
     @user.destroy
     respond_with @user
@@ -46,6 +45,6 @@ class UsersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.require(:user).permit(:name, :password)
+      params.require(:user).permit(:name, :password, :email)
     end
 end

eff58ac7802f7700e32c1ea67109bcb15486dc1a
Bennett Goble <Bennett.Goble@careerbuilder.com>
2013-12-04 18:27:18 -0500

    Save tenant functionality

--- app/controllers/tenants_controller.rb
+++ app/controllers/tenants_controller.rb
@@ -14,13 +14,13 @@ class TenantsController < ApplicationController
 
   # GET /organizations/:org_id/tenants
   def by_organization
-    @tenants = Tenant.find(params[:org_id]).children
+    @tenants = params[:include_root] ? [Tenant.find(params[:org_id])] : Tenant.find(params[:org_id]).children
     render :index
   end
 
   # GET /organizations/:org_id/tenants/hierarchy
   def hierarchy_by_organization
-    @tenants = Tenant.find(params[:org_id]).children
+    @tenants = params[:include_root] ? [Tenant.find(params[:org_id])] : Tenant.find(params[:org_id]).children
     render :hierarchy
   end
 
@@ -31,7 +31,9 @@ class TenantsController < ApplicationController
 
   # POST /tenants
   def create
-    respond_with Tenant.new(tenant_params)
+    @tenant = Tenant.new(tenant_params)
+    @tenant.save
+    respond_with @tenant
   end
 
   # PATCH/PUT /tenants/1
@@ -53,6 +55,6 @@ class TenantsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def tenant_params
-      params.require(:tenant).permit(:name, :parent_id, :contact_name, :contact_email, :contact_phone, :active_at, :inactive_at, :contract, :did)
+      params.require(:tenant).permit(:name, :parent_id, :contact_name, :contact_email, :contact_phone, :active_at, :deactive_at, :contract, :did)
     end
 end
--- app/views/tenants/_tenant.json.jbuilder
+++ app/views/tenants/_tenant.json.jbuilder
@@ -1,4 +1,4 @@
-json.extract! tenant, :name, :id, :contact_name, :contact_email, :contact_phone, :created_at, :deleted_at, :updated_at, :contract, :did
+json.extract! tenant, :name, :id, :contact_name, :contact_email, :contact_phone, :created_at, :deleted_at, :deactive_at, :active_at, :updated_at, :contract, :did
 json.url tenant_url(tenant, format: :json)
 json.parent_url tenant.parent_id ? tenant_url(tenant.parent_id, format: :json) : nil
 json.parent_id tenant.parent_id

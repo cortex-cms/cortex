043ac1704cf22ebafc02fec6e2e050a59df82cde
Alex Tharp <alex.tharp@careerbuilder.com>
2013-12-06 11:06:19 -0500

    A test to see if this resolves things!

--- config/application.rb
+++ config/application.rb
@@ -5,7 +5,7 @@ require 'rails/all'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(:default, Rails.env)
-
+config.assets.initialize_on_precompile = false
 module Cortex
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.

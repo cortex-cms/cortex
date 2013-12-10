a75d7c93bcee96e70ff61c33f08861a74e45f654
Bennett Goble <Bennett.Goble@careerbuilder.com>
2013-12-10 14:29:02 -0500

    Fixed content-type validation for asset attachment

--- app/models/asset.rb
+++ app/models/asset.rb
@@ -1,23 +1,23 @@
 class Asset < ActiveRecord::Base
   belongs_to :user
   acts_as_taggable
-  has_attached_file :attachment, :styles => { :medium => "300x300>", :thumb => "100x100>" }
+  has_attached_file :attachment, :styles => { :medium => '300x300>', :thumb => '100x100>' }
 
   validates_attachment :attachment, :presence => true,
-  	:content_type => {
-    	:content_type => "image/jpeg",
-    	:content_type => "image/png",
-    	:content_type => "application/pdf",
-    	:content_type => "application/vnd.ms-excel",
-    	:content_type => "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
-    	:content_type => "application/msword",
-    	:content_type => "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
-    	:content_type => "text/plain",
-    	:content_type => "image/gif",
-    	:content_type => "application/zip",
-    	:content_type => "video/x-msvideo",
-    	:content_type => "video/quicktime",
-    	:content_type => "video/mp4"
-	},
-    :size => { :in => 0..10.megabytes } 
+  	:content_type => { :content_type => [
+    	'image/jpeg',
+    	'image/png',
+    	'application/pdf',
+    	'application/vnd.ms-excel',
+    	'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+    	'application/msword',
+    	'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
+    	'text/plain',
+    	'image/gif',
+    	'application/zip',
+    	'video/x-msvideo',
+    	'video/quicktime',
+    	'video/mp4']
+	  },
+    :size => { :in => 0..10.megabytes }
 end

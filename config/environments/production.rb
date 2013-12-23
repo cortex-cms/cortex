3c87ffd587da5796df93110fafe038c0bc5e1ebc
Alex Tharp <alex.tharp@careerbuilder.com>
2013-12-23 16:24:55 -0500

    Set Paperclip config for Production environment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,10 @@ Cortex::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  Paperclip.options[:command_path] = "/usr/local/bin/"
+  Paperclip::Attachment.default_options[:storage] = :fog
+  Paperclip::Attachment.default_options[:fog_credentials] = {:provider => "Local", :local_root => "#{Rails.root}/public"}
+  Paperclip::Attachment.default_options[:fog_directory] = ""
+  Paperclip::Attachment.default_options[:fog_host] = "http://whale.cb-cortex.c66.me"
 end

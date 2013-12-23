4f0b59bc6c2b9aff080f72b2a97f769f383d23a8
Bennett Goble <bennett.goble@careerbuilder.com>
2013-12-23 13:28:49 -0500

    Post Seeds

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,8 @@ class Post < ActiveRecord::Base
   has_and_belongs_to_many :assets
   has_and_belongs_to_many :categories
   belongs_to :user
+
+  self.inheritance_column = nil
 end
 
 # == Schema Information

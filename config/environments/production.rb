b0b82f90a0c3e169a4fa212926bb4414d50b8827
Alex Tharp <alex.tharp@careerbuilder.com>
2013-12-30 13:32:45 -0500

    Add Production Searchly URL to Production Tire config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,3 +84,7 @@ Cortex::Application.configure do
   Paperclip::Attachment.default_options[:fog_directory] = ""
   Paperclip::Attachment.default_options[:fog_host] = "http://whale.cb-cortex.c66.me"
 end
+
+Tire.configure do
+  url 'http://site:9d84bce49d97b603ae9b306068f27a83@api.searchbox.io/'
+end

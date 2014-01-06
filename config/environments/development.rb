8004a11c8b1540bd24945743099d19eb0fcf8449
Alex Tharp <alex.tharp@careerbuilder.com>
2014-01-06 15:36:14 -0500

    Create Staging environment configurations

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -17,7 +17,7 @@ Cortex::Application.configure do
   config.action_mailer.raise_delivery_errors = false
 
   # Print deprecation notices to the Rails logger.
-  config.active_support.deprecation = :log
+  #config.active_support.deprecation = :log
 
   # Raise an error on page load if there are pending migrations
   config.active_record.migration_error = :page_load

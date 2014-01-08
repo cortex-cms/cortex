9697fb618684c7ca69d31e0bad01a3ffef7009f5
Alex Tharp <alex.tharp@careerbuilder.com>
2014-01-08 11:50:02 -0500

    Reformat models/asset.rb, make image? private

--- app/models/asset.rb
+++ app/models/asset.rb
@@ -14,51 +14,51 @@ class Asset < ActiveRecord::Base
   before_save :generate_digest
 
   has_attached_file :attachment, :styles => {
-    :large   => {geometry: '800x800>', format: :jpg},
-    :default => {geometry: '300x300>', format: :jpg},
-    :mini    => {geometry: '100x100>', format: :jpg},
-    :micro   => {geometry: '50x50>', format: :jpg}
+      :large => {geometry: '800x800>', format: :jpg},
+      :default => {geometry: '300x300>', format: :jpg},
+      :mini => {geometry: '100x100>', format: :jpg},
+      :micro => {geometry: '50x50>', format: :jpg}
   }
 
   validates_attachment :attachment, :presence => true,
-  	:content_type => { :content_type => AppSettings.assets.allowed_media_types },
-    :size => { :in => 0..AppSettings.assets.max_size_mb.megabytes }
+                       :content_type => {:content_type => AppSettings.assets.allowed_media_types},
+                       :size => {:in => 0..AppSettings.assets.max_size_mb.megabytes}
 
   mapping do
-    indexes :id,          :index => :not_analyzed
-    indexes :name,        :analyzer => :snowball, :boost => 2.0
-    indexes :created_by,  :analyzer => :keyword, :as => 'user.name'
-    indexes :file_name,   :analyzer => :keyword
+    indexes :id, :index => :not_analyzed
+    indexes :name, :analyzer => :snowball, :boost => 2.0
+    indexes :created_by, :analyzer => :keyword, :as => 'user.name'
+    indexes :file_name, :analyzer => :keyword
     indexes :description, :analyzer => :snowball
-    indexes :tags,        :analyzer => :keyword, :as => 'tag_list'
-    indexes :created_at,  :type => :date, :include_in_all => false
+    indexes :tags, :analyzer => :keyword, :as => 'tag_list'
+    indexes :created_at, :type => :date, :include_in_all => false
   end
 
   class << self
     remove_method :index
   end
 
+  private
+
   def image?
     attachment_content_type =~ %r{^(image|(x-)?application)/(bmp|gif|jpeg|jpg|pjpeg|png|x-png)$}
   end
 
-  private
-
-    def extract_dimensions
-      return unless image?
-      tempfile = attachment.queued_for_write[:original]
-      unless tempfile.nil?
-        geometry = Paperclip::Geometry.from_file(tempfile)
-        self.dimensions = [geometry.width.to_i, geometry.height.to_i]
-      end
+  def extract_dimensions
+    return unless image?
+    tempfile = attachment.queued_for_write[:original]
+    unless tempfile.nil?
+      geometry = Paperclip::Geometry.from_file(tempfile)
+      self.dimensions = [geometry.width.to_i, geometry.height.to_i]
     end
+  end
 
-    def generate_digest
-      tempfile = attachment.queued_for_write[:original]
-      unless tempfile.nil?
-        self.digest = Digest::SHA1.file(tempfile.path).to_s
-      end
+  def generate_digest
+    tempfile = attachment.queued_for_write[:original]
+    unless tempfile.nil?
+      self.digest = Digest::SHA1.file(tempfile.path).to_s
     end
+  end
 end
 
 # == Schema Information

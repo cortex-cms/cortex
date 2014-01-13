092fb437aaf757356ba047f9dbe175d416c8515b
Bennett Goble <bennett.goble@careerbuilder.com>
2014-01-13 12:26:50 -0500

    Added taxon index

--- app/models/concerns/taxon.rb
+++ app/models/concerns/taxon.rb
@@ -1,14 +1,16 @@
 module Taxon 
-	extend ActiveSupport::Concern 
+	extend ActiveSupport::Concern
 
 	def taxon
 		main_type = self.class.name[0]
 
 		taxon_date = created_at.strftime("%y%m%d")
 
-		taxon_hex = SecureRandom.hex[0..2]
+		"#{main_type}#{taxon_subtype}#{taxon_date}#{taxon_created_at_index}"
+  end
 
-		"#{main_type}#{taxon_subtype}#{taxon_date}#{taxon_hex}"
-	end
+  def taxon_created_at_index
+    '%04x' % self.class.with_deleted.where(created_at: Time.new(created_at.year, created_at.month, created_at.day)..created_at).count
+  end
 
 end

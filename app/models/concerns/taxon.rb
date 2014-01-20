bb78d2713416b224d21a09476bb57c61e530049e
Carlos Gonzalez <carlos.gonzalez@careerbuilder.com>
2014-01-20 16:48:09 -0500

    changed main_type to module_type and taxon_subtype to taxon_type at models/concerns/taxon.rb

--- app/models/concerns/taxon.rb
+++ app/models/concerns/taxon.rb
@@ -2,13 +2,14 @@ module Taxon
 	extend ActiveSupport::Concern
 
 	def taxon
-		main_type = self.class.name.first
+	  module_type = self.class.name.first
 		taxon_date = created_at.strftime("%y%m%d")
 
-		"#{main_type}#{taxon_subtype}#{taxon_date}#{taxon_created_at_index}"
+		"#{module_type}#{taxon_type}#{taxon_date}#{taxon_created_at_index}"
   end
 
   def taxon_created_at_index
+    # Returns count of assets created that day as hex
     '%04x' % self.class.with_deleted.where(created_at: Time.new(created_at.year, created_at.month, created_at.day)..created_at).count
   end
 

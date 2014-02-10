7a0782f1e742a1e1b0191b65c928c2609763dd63
Bennett Goble <bennett.goble@careerbuilder.com>
2014-02-10 15:29:46 -0500

    Use tag_list to persist tags

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,10 +1,10 @@
 class CategoriesController < ApplicationController
   def index
     @categories = Category.all
+    render :hierarchy
   end
 
   def hierarchy
     @categories = params[:roots_only] == 'false' ? Category.all : Category.roots
-    render :hierarchy
   end
 end

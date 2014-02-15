0706601dd365742bef9af1cb85d5ad3cf216b9fd
Carlos Gonzalez <carlos.gonzalez@careerbuilder.com>
2014-02-14 19:01:37 -0500

    Modified api/v1/entities/post.rb to accept :display param from Post form

--- app/api/v1/entities/post.rb
+++ app/api/v1/entities/post.rb
@@ -2,8 +2,12 @@ module API::V1
   module Entities
     class Post < Grape::Entity
       expose :id, :copyright_owner, :author, :title, :type, :published_at, :expired_at, :deleted_at, :created_at,
-             :updated_at, :draft, :comment_count, :body, :short_description, :job_phase, :display, :featured_image_url,
+             :updated_at, :draft, :comment_count, :body, :short_description, :job_phase, :featured_image_url,
              :notes, :seo_title, :seo_description, :seo_preview
+
+      expose(:display) do |post|
+        post.display
+      end
 
       expose(:tags) { |post| post.tag_list.join(', ') }
       expose :user, with: 'Entities::UserBasic'

25ea83b8a024532fb6b443f4f62b2192ed14bfd6
Alex Tharp <alex.tharp@careerbuilder.com>
2014-03-10 13:32:42 -0400

    Remove extraneous add_user_localization migration

--- db/schema.rb
+++ db/schema.rb
@@ -164,15 +164,15 @@ ActiveRecord::Schema.define(version: 20140305183048) do
   add_index "tenants", ["parent_id"], name: "index_tenants_on_parent_id", using: :btree
 
   create_table "users", force: true do |t|
-    t.string   "email",                  default: "", null: false
+    t.string   "email",                             default: "",      null: false
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "tenant_id",                           null: false
-    t.string   "encrypted_password",     default: "", null: false
+    t.integer  "tenant_id",                                           null: false
+    t.string   "encrypted_password",                default: "",      null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer  "sign_in_count",                     default: 0,       null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"

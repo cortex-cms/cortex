9c7ba7164613ef711f70515a50f42dbd13769cdd
Alex Tharp <alex.tharp@careerbuilder.com>
2014-03-13 18:22:48 -0400

    Remove apartment tasks from cloud66 bootstrap script

--- .cloud66/scripts/bootstrap_db.sh
+++ .cloud66/scripts/bootstrap_db.sh
@@ -1,7 +1,4 @@
 bundle exec rake db:migrate
 bundle exec rake db:seed
 
-bundle exec rake apartment:migrate
-bundle exec rake apartment:seed
-
 bundle exec rake environment tire:import CLASS='Media' FORCE=true

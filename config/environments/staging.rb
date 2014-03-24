57b3f62cff3ac0b8c7d89ec000aba2e474197197
Alex Tharp <alex.tharp@careerbuilder.com>
2014-03-24 12:46:47 -0400

    Update staging.rb

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -82,7 +82,7 @@ Cortex::Application.configure do
   Paperclip::Attachment.default_options[:storage] = :fog
   Paperclip::Attachment.default_options[:fog_credentials] = {:provider => "Local", :local_root => "#{Rails.root}/public"}
   Paperclip::Attachment.default_options[:fog_directory] = ""
-  Paperclip::Attachment.default_options[:fog_host] = "http://staging.api.cbcortex.com"
+  Paperclip::Attachment.default_options[:fog_host] = "http://stg.api.cbcortex.com"
 
   Sidekiq.configure_server do |config|
     config.redis = { :namespace => 'cortex_stg' }

16b211ade2c238243f549a9fc132debf46c176eb
Bennett Goble <bennett.goble@careerbuilder.com>
2014-03-26 16:37:18 -0400

    Gravatar URL added to user :milky_way:

--- app/api/v1/entities/user.rb
+++ app/api/v1/entities/user.rb
@@ -1,9 +1,13 @@
+require 'digest/md5'
 require_relative 'user_basic'
 
 module API::V1
   module Entities
     class User < UserBasic
       expose :email, :created_at, :updated_at, :sign_in_count, :firstname, :lastname, :fullname, :tenant_id, :profile
+      expose :gravatar do |user|
+        "//www.gravatar.com/avatar/#{Digest::MD5.hexdigest user.email}"
+      end
     end
   end
 end

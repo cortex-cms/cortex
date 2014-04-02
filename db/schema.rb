b4de5ae4df8705d82a5e2583e50c49f8dc278317
Alex Tharp <alex.tharp@careerbuilder.com>
2014-04-02 14:11:18 -0400

    Rollback acts-as-taggable-on upgrade, rollback migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140402155854) do
+ActiveRecord::Schema.define(version: 20140401042719) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -136,8 +136,7 @@ ActiveRecord::Schema.define(version: 20140402155854) do
   add_index "taggings", ["tag_id", "taggable_id", "taggable_type", "context", "tagger_id", "tagger_type"], name: "taggings_idx", unique: true, using: :btree
 
   create_table "tags", force: true do |t|
-    t.string  "name"
-    t.integer "taggings_count", default: 0
+    t.string "name"
   end
 
   add_index "tags", ["name"], name: "index_tags_on_name", unique: true, using: :btree

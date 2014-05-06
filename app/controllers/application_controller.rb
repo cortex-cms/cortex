260a759899b844b728dd372d1a2abd95075f60ad
Bennett Goble <nivardus@gmail.com>
2014-05-06 13:01:26 -0400

    Disable auth while troubleshooting angular-rails-templates

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :default_headers
-  before_action :authenticate_user!
+  #before_action :authenticate_user!
   before_action :add_gon
   before_action :configure_permitted_parameters, if: :devise_controller?
 

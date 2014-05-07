c4ced191279a7273c811d880970e82eb08c6c076
Bennett Goble <nivardus@gmail.com>
2014-05-07 12:06:20 -0400

    Tweak deployment script hooks

---
+++ .cloud66/after_bundle.sh
@@ -0,0 +1,4 @@
+bundle exec rake bower:install
+
+# Correct permissions for assets
+sudo chmod 0777 -R $RAILS_STACK_PATH/public/*
--- .cloud66/after_rails.sh
+++ .cloud66/after_rails.sh
@@ -7,8 +7,3 @@ bundle exec rake db:seed
 
 bundle exec rake environment tire:import CLASS='Media' FORCE=true
 bundle exec rake environment tire:import CLASS='Post' FORCE=true
-
-bundle exec rake bower:install
-
-# Correct permissions for assets
-sudo chmod 0777 -R $RAILS_STACK_PATH/public/*

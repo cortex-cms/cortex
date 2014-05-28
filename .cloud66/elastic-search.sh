599b5e40f31a16fc3b5d2c434cd53ff5c50a55cd
Alex Tharp <alex.tharp@careerbuilder.com>
2014-05-28 14:44:33 -0400

    Add Post's Industry to the ES index

--- .cloud66/elastic-search.sh
+++ .cloud66/elastic-search.sh
@@ -4,3 +4,4 @@ cd $STACK_PATH
 
 bundle exec rake environment tire:import CLASS='Media' FORCE=true
 bundle exec rake environment tire:import CLASS='Post' FORCE=true
+bundle exec rake environment tire:import CLASS='Onet::Occupation' FORCE=true
--- app/models/post.rb
+++ app/models/post.rb
@@ -41,6 +41,7 @@ class Post < ActiveRecord::Base
     indexes :categories,        :analyzer => :keyword, :as => 'categories.collect{ |c| c.name }'
     indexes :job_phase,         :analyzer => :keyword
     indexes :type,              :analyzer => :keyword, :as => 'type'
+    indexes :industry,          :analyzer => :keyword, :as => 'industry.title'
   end
 
   def published?

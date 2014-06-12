8596790b6d95306a6d8917902669b001e1b2cf2c
Bennett Goble <nivardus@gmail.com>
2014-06-12 10:41:43 -0400

    Expose post.display at runtime

--- app/api/v1/entities/post_basic.rb
+++ app/api/v1/entities/post_basic.rb
@@ -4,7 +4,12 @@ module API::V1
       expose :id, :copyright_owner, :author, :title, :type, :published_at, :expired_at, :deleted_at,
              :created_at, :updated_at, :draft, :comment_count, :body, :short_description,
              :job_phase, :notes, :seo_title, :seo_description, :seo_preview, :slug,
-             :primary_category_id, :primary_industry_id, :display
+             :primary_category_id, :primary_industry_id
+
+      # This runtime exposure is necessary to correctly resolve the enum value
+      expose(:display) do |post|
+        post.display
+      end
 
       expose :categories, with: 'Entities::Category'
       expose :featured_media, with: 'Entities::MediaFeatured'

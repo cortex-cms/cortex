14431d368eb8247da95ae204bb1ee5d5bcfd2271
Bennett Goble <nivardus@gmail.com>
2014-06-18 16:59:53 -0400

    Update youtube worker to use hash

--- app/workers/youtube_media_worker.rb
+++ app/workers/youtube_media_worker.rb
@@ -8,16 +8,16 @@ class YoutubeMediaWorker
 
     info = YoutubeHelper::fetch_info(media.video_id)
 
-    media.url                 = info.url
-    media.title               = info.title
-    media.authors             = info.authors
-    media.duration            = info.duration
-    media.source_published_at = info.published
-    media.source_updated_at   = info.updated
-    media.video_description   = info.description
+    media.url                 = info[:url]
+    media.title               = info[:title]
+    media.authors             = info[:authors]
+    media.duration            = info[:duration]
+    media.source_published_at = info[:published]
+    media.source_updated_at   = info[:updated]
+    media.video_description   = info[:description]
 
     # Fetch thumbnail
-    r = Excon.get(info.thumbnail)
+    r = Excon.get(info[:thumbnail])
 
     if r.status >= 300; raise "Error while requesting Youtube thumbnail (#{r.status}) r.body" end
 

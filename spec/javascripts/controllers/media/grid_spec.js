70841d727923b95017767fcd3dd4fa4ed3570292
Bennett Goble <nivardus@gmail.com>
2014-06-25 13:45:46 -0400

    Media Grid specs

--- spec/javascripts/controllers/media/grid_spec.js
+++ spec/javascripts/controllers/media/grid_spec.js
@@ -10,12 +10,32 @@
     });
 
     describe('MediaGridCtrl', function($rootScope) {
-      var createController, $scope;
+      var createController, $scope, $stateParams, $state, $window, settings, media, cortex;
 
-      beforeEach(inject(function($controller, $rootScope, $state, $stateParams, _$httpBackend_,
-                                 cortex, settings, mediaSelectType, flash, PostBodyEditorService,
-                                 PostsPopupService) {
-        $scope = $rootScope.$new();
+      beforeEach(inject(function($controller, $rootScope, _$state_, _$httpBackend_, _cortex_,
+                                 _settings_, mediaSelectType, flash, PostBodyEditorService,
+                                 PostsPopupService, _$window_) {
+        $scope       = $rootScope.$new();
+        $stateParams = {};
+        $state       = _$state_;
+        $window      = _$window_;
+        settings     = _settings_;
+        cortex       = _cortex_;
+        media        = [{id: 1}, {id: 2}];
+
+        // Mocked media response
+        cortex.media.searchPaged = function(args, successFn) {
+          var paging = {per_page: 10, page: 1};
+          if (args) {
+            paging = angular.extend(paging, args);
+          }
+
+          if (successFn) {
+            successFn(media, {}, paging);
+          }
+
+          return media;
+        };
 
         createController = function() {
           return $controller('MediaGridCtrl', {
@@ -36,13 +56,73 @@
         expect(createController()).toBeTruthy();
       });
 
-      it('should provide page');
+      describe('$scope.page', function() {
 
-      it('should provide data.media');
+        it('should provide query', function() {
+          $stateParams.query = 'query';
+          createController();
+          expect($scope.page.query).toEqual($stateParams.query);
+        });
 
-      it('should provide deleteMedia()');
+        it('should use $stateParams.page if available', function() {
+          $stateParams.page = 2;
+          createController();
+          expect($scope.page.page).toEqual($stateParams.page);
+        });
 
-      it('should update media when page changes');
+        it('should provide default page', function() {
+          createController();
+          expect($scope.page.page).toEqual(1);
+        });
+
+        it('should use $stateParams.page if available', function() {
+          $stateParams.page = 2;
+          createController();
+          expect($scope.page.page).toEqual($stateParams.page);
+        });
+
+        it('should provide default perPage', function() {
+          createController();
+          expect($scope.page.perPage).toEqual(settings.paging.defaultPerPage);
+        });
+
+        it('should use $stateParams.perPage if available', function() {
+          $stateParams.perPage = 23;
+          createController();
+          expect($scope.page.perPage).toEqual($stateParams.perPage);
+        });
+
+        it('should provide next()', function() {
+          $stateParams.page = 1;
+          createController();
+          $state.go = angular.noop;
+          $scope.page.next();
+          expect($scope.page.page).toEqual(2);
+        });
+
+        it('should provide previous()', function() {
+          $stateParams.page = 2;
+          createController();
+          $state.go = angular.noop;
+          $scope.page.previous();
+          expect($scope.page.page).toEqual(1);
+        });
+      });
+
+      it('should provide data.media', function() {
+        createController();
+        expect($scope.data.media.length).toEqual(2);
+      });
+
+      it('should provide deleteMedia()', function() {
+        $window.confirm = function() { return true; }
+        cortex.media.delete = function(args, successFn) {
+          successFn();
+        };
+        createController();
+        $scope.deleteMedia(media[0])
+        expect($scope.data.media).toNotContain(media[0]);
+      });
     });
   });
 })();

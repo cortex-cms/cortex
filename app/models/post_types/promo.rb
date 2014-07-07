718a16103c21167c432c680d0e0199ca21b001f6
Colin Ewen <draecas@gmail.com>
2014-07-07 19:28:12 -0400

    Add explicit enum for display size

--- app/models/post_types/promo.rb
+++ app/models/post_types/promo.rb
@@ -3,6 +3,9 @@ class Promo < Post
   document_type    'posts'
   store_accessor :meta, :destination_url, :call_to_action
 
+  # TODO: Figure out a way to get this properly abstracted
+  enum display: [:large, :medium, :small]
+
   validates :destination_url, :call_to_action, presence: true, allow_nil: false
 
 end

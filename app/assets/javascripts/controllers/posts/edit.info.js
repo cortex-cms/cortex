397ae782be1e01ade36265737b05912ced426c9e
Colin Ewen <colin@draecas.com>
2014-07-16 10:28:32 -0400

    Move the slug check to a delayed bind (no point making 3000 requests as people type)

--- app/assets/javascripts/controllers/posts/edit.info.js
+++ app/assets/javascripts/controllers/posts/edit.info.js
@@ -1,9 +1,10 @@
 angular.module('cortex.controllers.posts.edit.info', [
   'cortex.services.cortex',
-  'cortex.filters'
+  'cortex.filters',
+  'cortex.util'
 ])
 
-.controller('PostsEditInfoCtrl', function($scope, $filter, cortex) {
+.controller('PostsEditInfoCtrl', function($scope, $filter, cortex, delayedBind) {
 
     // Auto-generate slug when title changed and field isn't dirty
     $scope.$watch('data.post.title', function(title) {
@@ -11,33 +12,36 @@ angular.module('cortex.controllers.posts.edit.info', [
         return;
       }
       $scope.data.post.slug = $filter('slugify')($scope.data.post.title);
+
     });
 
-    $scope.$watch('data.post.slug', function(slug) {
-
+    function slugCheck() {
+      var slug = $scope.data.post.slug;
       if (!slug) {
         return;
       }
 
       cortex.posts.get({id: slug},
-        // Slug already used
-        function(post) {
+          // Slug already used
+          function(post) {
 
-          // A post may have its own slug
-          if (post.id === $scope.data.post.id) {
+            // A post may have its own slug
+            if (post.id === $scope.data.post.id) {
+              $scope.postForm.slug.$error.unavailable = false;
+              $scope.data.postWithDuplicateSlug = null;
+            }
+            else {
+              $scope.postForm.slug.$error.unavailable = true;
+              $scope.data.postWithDuplicateSlug = post;
+            }
+          },
+          // Slug unused
+          function() {
             $scope.postForm.slug.$error.unavailable = false;
             $scope.data.postWithDuplicateSlug = null;
           }
-          else {
-            $scope.postForm.slug.$error.unavailable = true;
-            $scope.data.postWithDuplicateSlug = post;
-          }
-        },
-        // Slug unused
-        function() {
-          $scope.postForm.slug.$error.unavailable = false;
-          $scope.data.postWithDuplicateSlug = null;
-        }
       );
-    });
+    }
+      
+    delayedBind($scope, 'data.post.slug', 1000, slugCheck);
 });

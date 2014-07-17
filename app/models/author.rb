7df4d046d82546c3fab41cee3bc8142cf9714cbe
Bennett Goble <nivardus@gmail.com>
2014-07-16 20:21:12 -0400

    Fix Author.published where scope

--- app/models/author.rb
+++ app/models/author.rb
@@ -2,7 +2,7 @@ class Author < ActiveRecord::Base
   include HasGravatar
   include HasFirstnameLastname
 
-  scope :published, -> { joins(:posts).where('user_id IS NOT NULL AND posts.published_at <= ?', DateTime.now) }
+  scope :published, -> { joins(:posts).where('authors.user_id IS NOT NULL AND posts.published_at <= ?', DateTime.now) }
 
   store_accessor :sites, :personal, :facebook, :twitter, :google
   belongs_to :user

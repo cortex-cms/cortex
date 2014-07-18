8df9148e3eb1e91e0296dea7554acf1b6325200e
Bennett Goble <nivardus@gmail.com>
2014-07-18 14:21:02 -0400

    Added API minimal tests for /users/:id/author

--- spec/models/concerns/has_gravatar_spec.rb
+++ spec/models/concerns/has_gravatar_spec.rb
@@ -6,5 +6,10 @@ describe HasGravatar do
       author = build(:author, email: 'talentsolutionstechnology@careerbuilder.com')
       author.avatars[:default].should include('211d03ded8ca5767c12c5f80f9cb6ad2')
     end
+
+    it 'should return the default URL if there is no email' do
+      author = build(:author, email: nil)
+      author.avatars[:default].should include('00000000000000000000000000000000')
+    end
   end
 end

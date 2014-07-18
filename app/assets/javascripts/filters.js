93cd7dc65d3a990e7d90b7dc6c934d629b040364
Alex Tharp <alex.tharp@careerbuilder.com>
2014-07-18 00:20:50 -0400

    Fix Add Media to Post (D-07298) + bundle update

--- app/assets/javascripts/filters.js
+++ app/assets/javascripts/filters.js
@@ -15,19 +15,19 @@ angular.module('cortex.filters', [
   return function(media) {
     var outputHtml;
 
-    if (media.general_type ==='image') {
+    if (media.type === 'image') {
       outputHtml = '<p><a href="' +
-        media.attachment_url +
+        media.url +
         '"><img alt="' +
         (media.alt || '') +
         '" src="' +
-        media.attachment_url +
+        media.url +
         '" data-media-id="' +
         media.id +
         '"></a></p>';
     }
     else {
-      outputHtml = '<a href=""' +
+      outputHtml = '<a href="' +
         media.attachment_url +
         '" data-media-id="' +
         media.id +

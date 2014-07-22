ba62a7322b54d0fa51e96fb4eba72485137ab4c1
Colin Ewen <colin@draecas.com>
2014-07-22 14:10:05 -0400

    Media changes

--- app/api/v1/entities/media_basic.rb
+++ app/api/v1/entities/media_basic.rb
@@ -8,7 +8,7 @@ module API::V1
       expose :dimensions, documentation: { type: "Array", desc: "Array of [Width, Height]" }
       expose :url, documentation: { type: "String", desc: "URL of Media" }
       expose :type, documentation: { type: "String", desc: "Media Type" }
-      expose :attachment, using: 'Entities::MediaThumbnails', as: :thumbs, if: lambda { |media, _| media.can_thumb }, documentation: { type: "MediaThumbnail", desc: "Thumbnails of the media"}
+      expose :attachment, using: 'Entities::MediaThumbnails', as: :thumbs, if: lambda { |media, _| media.can_thumb }, documentation: { type: "MediaThumbnails", desc: "Thumbnails of the media"}
 
       expose :video_id, if: lambda { |media, _| media.content_type == 'youtube' }, documentation: { type: "String", desc: "Youtube Video ID"}
     end

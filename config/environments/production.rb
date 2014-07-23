3116c8812c1e12d0e783a3b7c2916bafd47d3b91
Bennett Goble <nivardus@gmail.com>
2014-07-23 14:06:54 -0400

    Paperclip prod config - CNAME for Cloudfront

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,7 +85,9 @@ Cortex::Application.configure do
     :s3_credentials => {
       :bucket            => ENV['S3_BUCKET_NAME'],
       :access_key_id     => ENV['AWS_ACCESS_KEY_ID'],
-      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'],
+      :s3_host_alias     => ENV['S3_HOST_ALIAS'],
+      :url               => ':s3_alias_url'
     }
   }
 

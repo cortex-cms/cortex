8973ff70d7b82c82a2935dd8cd2a03842991eba8
Alex Tharp <alex.tharp@careerbuilder.com>
2014-07-28 17:51:26 -0400

    Initial functionality for exposing Post entity with sanitized body using format_with. No tests yet.

--- app/api/v1/entities/post.rb
+++ app/api/v1/entities/post.rb
@@ -2,14 +2,12 @@ module API
   module V1
     module Entities
       class Post < Grape::Entity
-
         expose :id, documentation: {type: "Integer", desc: "Post ID", required: true}
         expose :title, documentation:  {desc: "Post Title", type: "String" , required: true}
         expose :custom_author, documentation: {desc: "Ad hoc author", type: "String", required: true}
         expose :type, documentation: {desc: "Post Type", type: "String", enum: ["ArticlePost", "VideoPost", "InfographicPost", "PromoPost"], required: true}
         expose :draft, documentation: {desc: "Draft status", type: "Boolean"}
         expose :comment_count, documentation: {desc: "Number of comments", type: "Integer"}
-        expose :body, documentation: {desc: "Body of the post", type: "String"}
         expose :short_description, documentation: {desc: "Short description of the post", type: "String", required: true}
         expose :job_phase, documentation: {desc: "Job Phase", type: "String", enum: ["discovery", "find_the_job", "get_the_job", "on_the_job"], required: true}
         expose :slug, documentation: {desc: "Slug", type: "String", required: true}
@@ -24,18 +22,25 @@ module API
         expose :primary_category_id, documentation: {type: "Integer", desc: "Primary Category ID"}
         expose :primary_industry_id, documentation: {type: "Integer", desc: "Primary Industry ID"}
 
-        # This runtime exposure is necessary to correctly resolve the enum value
-        expose :display, {documentation: { type: "String", desc: "Post Display Size"}} do |post|
-          post.display
-        end
-
         expose :categories, using: 'Entities::Category', documentation: {type: 'Category', is_array: true, desc: "Categories"}
         expose :featured_media, using: 'Entities::Media', documentation: {type: 'Media', is_array: false, desc: "Featured Media for this post"}
         expose :tile_media, using: 'Entities::Media', documentation: {type: 'Media', is_array: false, desc: "Tile Media for this post"}
         expose :industries, using: 'Entities::Occupation', documentation: {type: "Industry", is_array: true, desc: "Industries"}
 
-        expose :destination_url, { documentation: { type: "String", desc: "Destination URL for a Promo Post"}, if: lambda { |post, _| post.type == 'PromoPost' } }
-        expose :call_to_action, { documentation: { type: "String", desc: "Call to Action for a Promo Post"}, if: lambda { |post, _| post.type == 'PromoPost' } }
+        # This runtime exposure is necessary to correctly resolve the enum value
+        expose :display, {documentation: { type: "String", desc: "Post Display Size"}} do |post|
+          post.display
+        end
+
+        format_with(:sanitize_body) { |body| Sanitize.fragment(body, Cortex.config.sanitize_whitelist.post) }
+        with_options(format_with: :sanitize_body) do
+          expose :body, documentation: {desc: "Body of the post (sanitized)", type: "String"}
+        end
+
+        with_options if: lambda { |post, _| post.type == 'PromoPost' } do
+          expose :destination_url, { documentation: { type: "String", desc: "Destination URL for a Promo Post"} }
+          expose :call_to_action, { documentation: { type: "String", desc: "Call to Action for a Promo Post"} }
+        end
 
         with_options if: { full: true } do
           represent :media, with: 'Entities::Media', full: true, documentation: {type: "Media", is_array: true, desc: "All Media for this post"}

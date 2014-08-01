9cb2c0f2b4ccedb1f750d3649784bc50134a02ef
Colin Ewen <colin@draecas.com>
2014-08-01 11:26:00 -0400

    Allow the passing of a user parameter and use that before current user (to support application post creation)

--- app/models/abilities/application_ability.rb
+++ app/models/abilities/application_ability.rb
@@ -3,12 +3,11 @@ module Abilities
     class << self
       def allowed(app)
         if app.write
-          %w[:view :create :update :delete]
+          [:view, :create, :update, :delete]
         else
           [:view]
         end
       end
-
     end
   end
 end

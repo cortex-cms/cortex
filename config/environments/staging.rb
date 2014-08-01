0a5c350fb5cd5863bc4930fc335f3341a5555e8a
Alex Tharp <alex.tharp@careerbuilder.com>
2014-08-01 10:11:01 -0400

    Add CACHE_URL env variable to point to the redis store

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -22,6 +22,8 @@ Cortex::Application.configure do
   # Raise an error on page load if there are pending migrations
   config.active_record.migration_error = :page_load
 
+  config.cache_store = :redis_store, ENV['CACHE_URL']
+
   # Debug mode disables concatenation and preprocessing of assets.
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.

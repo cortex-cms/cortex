53f41a36d71d0363f1e643164dd2239b9b8c26b4
Alex Tharp <alex.tharp@careerbuilder.com>
2014-08-04 16:38:18 -0400

    Use ELASTICSEARCH_ADDRESS env variable instead of default

--- config/initializers/elasticsearch.rb
+++ config/initializers/elasticsearch.rb
@@ -1,3 +1,5 @@
 # Ensure elasticsearch-model is hooked up with pagination
 Kaminari::Hooks.init
 Elasticsearch::Model::Response::Response.__send__ :include, Elasticsearch::Model::Response::Pagination::Kaminari
+
+Elasticsearch::Model.client = Elasticsearch::Client.new url: ENV['ELASTICSEARCH_ADDRESS']

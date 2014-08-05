e84f520fff94c0846be8b3b1e5d33e74ac06c1be
Alex Tharp <alex.tharp@careerbuilder.com>
2014-08-05 17:13:23 -0400

    Load Dotenv only for development env

--- config/application.rb
+++ config/application.rb
@@ -2,8 +2,6 @@ require File.expand_path('../boot', __FILE__)
 require 'rails/all'
 require 'elasticsearch/rails/instrumentation'
 Bundler.require(:default, Rails.env)
-Dotenv.load
-
 
 module Cortex
   class Application < Rails::Application

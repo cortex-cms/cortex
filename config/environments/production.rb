2c932a547e757eebb9dd02b330cb2af73e390d3c
Bennett Goble <nivardus@gmail.com>
2014-08-07 22:55:29 -0400

    Add default path value to paper_clip config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,6 +84,7 @@ Cortex::Application.configure do
     :storage        => :s3,
     :bucket         => ENV['S3_BUCKET_NAME'],
     :url            => ':s3_alias_url',
+    :path           => '/system/:class/:attachment/:id_partition/:style/:filename',
     :s3_host_alias  => ENV['S3_HOST_ALIAS'],
     :s3_credentials => {
       :access_key_id     => ENV['AWS_ACCESS_KEY_ID'],

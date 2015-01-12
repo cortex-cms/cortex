d87795845061c2b732885343dca0fafaa09db782
Colin Ewen <colin@draecas.com>
2015-01-12 12:09:54 -0500

    SPECS

--- spec/api/v1/resources/users_spec.rb
+++ spec/api/v1/resources/users_spec.rb
@@ -19,4 +19,41 @@ describe API::Resources::Users, :type => :request do
       expect(response.status).to eq(401)
     end
   end
+
+  describe 'PUT /users/:user_id' do
+    let(:user) { create(:user, password: "TestPassword") }
+    let(:other_user) { create(:user) }
+
+    context 'as the target user' do
+      before :each do
+        login_as user
+      end
+
+      it 'should reject if the current password is incorrect' do
+        put "/api/v1/users/#{user.id}", {current_password: "WrongPassword", password: "NewPassword", password_confirmation: "NewPassword"}
+        expect(response.status).to eq(403)
+      end
+
+      it 'should reject if the passwords don\'t match' do
+        put "/api/v1/users/#{user.id}", {current_password: "TestPassword", password: "NewPassword", password_confirmation: "Different Password"}
+        expect(response.status).to eq(422)
+      end
+
+      it 'should change the password when all is correct' do
+        put "/api/v1/users/#{user.id}", {current_password: "TestPassword", password: "NewPassword", password_confirmation: "NewPassword"}
+        expect(response.status).to eq(200)
+      end
+    end
+
+    context 'as a different user' do
+      before :each do
+        login_as other_user
+      end
+
+      it 'should reject in all cases' do
+        put "/api/v1/users/#{user.id}", {current_password: "TestPassword", password: "NewPassword", password_confirmation: "NewPassword"}
+        expect(response.status).to eq(403)
+      end
+    end
+  end
 end

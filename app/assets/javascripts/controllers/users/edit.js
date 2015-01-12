986e4efa9d60d1e4d7afc4d4a3a3874a443d633b
Colin Ewen <colin@draecas.com>
2015-01-12 10:42:50 -0500

    Password changing in

--- app/assets/javascripts/controllers/users/edit.js
+++ app/assets/javascripts/controllers/users/edit.js
@@ -1,8 +1,9 @@
 angular.module('cortex.controllers.users.edit', [
+  'ui.router.state',
   'angular-flash.service'
 ])
 
-.controller('UsersEditCtrl', function($scope, $anchorScroll, flash, user, author) {
+.controller('UsersEditCtrl', function($scope, $state, $anchorScroll, flash, user, author) {
   $scope.data        = $scope.data || {};
   $scope.data.user   = user;
   $scope.data.author = author;
@@ -18,5 +19,23 @@ angular.module('cortex.controllers.users.edit', [
         flash.error = 'Error while saving author information';
       }
     );
-  }
+  };
+  $scope.changePassword = function() {
+    $scope.data.user.$save().then(
+        function() {
+          $state.go('login');
+          flash.info = "Successfully updated password";
+        },
+        function(res) {
+          $anchorScroll();
+          if (res.status === 403) {
+            flash.error = "Current password is invalid";
+          } else if (res.data.errors[0] === "password_confirmation doesn't match Password") {
+            flash.error = "Passwords don't match.";
+          } else {
+            flash.error = "Could not change password, please try again'";
+          }
+        }
+    );
+  };
 });

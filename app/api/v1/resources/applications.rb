435b4af465903a56de5161b1e767d778bb4b777f
Colin Ewen <colin@draecas.com>
2015-01-14 09:29:46 -0500

    Resource skeleton

---
+++ app/api/v1/resources/applications.rb
@@ -0,0 +1,65 @@
+require_relative '../helpers/resource_helper'
+
+module API
+  module V1
+    module Resources
+      class Applications < Grape::API
+        helpers Helpers::SharedParams
+
+        resource :applications do
+          helpers Helpers::PaginationHelper
+
+
+          desc 'Show all applications', { entity: Entities::Application, nickname: 'showAllApplications' }
+          params do
+            use :pagination
+          end
+          get do
+            require_scope! :'view:applications'
+            authorize! :view, ::Application
+          end
+
+          desc 'Show an application', { entity: Entities::Application, nickname: "showApplication" }
+          get ':id' do
+            present application!, with: Entities::Application
+          end
+
+          desc 'Create an application', { entity: Entities::Application, params: Entities::Application.documentation, nickname: "createApplication" }
+          params do
+            requires :name, type: String, desc: "Application Name"
+          end
+          post do
+            require_scope! :'modify:tenants'
+            authorize! :create, Application
+
+            allowed_params = remove_params(Entities::Application.documentation.keys, :children)
+
+            @application = ::Application.new(declared(params, { include_missing: true }, allowed_params))
+            application.save!
+            present application, with: Entities::Application
+          end
+
+          desc 'Update an application', { entity: Entities::Application, params: Entities::Application.documentation, nickname: "updateApplication" }
+          put ':id' do
+            require_scope! :'modify:tenants'
+            authorize! :update, application!
+
+            allowed_params = remove_params(Entities::Application.documentation.keys, :children)
+
+            application.update!(declared(params, { include_missing: false }, allowed_params))
+            present application, with: Entities::Application
+          end
+
+          desc 'Delete an application', { nickname: "deleteApplication" }
+          delete ':id' do
+            require_scope! :'modify:tenants'
+            authorize! :delete, application!
+
+            application.destroy
+          end
+        end
+
+      end
+    end
+  end
+end

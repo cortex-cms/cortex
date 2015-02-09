a0e1522dd4aaf2741d7f9842684ac9c99a9aed20
Colin Ewen <colin@draecas.com>
2015-02-09 11:33:08 -0500

    Guardfile with interactors, spec helper undo the not working on travis bit

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -28,7 +28,7 @@ RSpec.configure do |config|
   config.include RSpec::Rails::RequestExampleGroup, type: :request, file_path: /spec\/api/
 
   config.before(:suite) do
-    DatabaseCleaner.strategy = :transaction, {except: %w[public.schema_migrations]}
+    DatabaseCleaner.strategy = :transaction
     DatabaseCleaner.clean_with(:truncation)
     elasticsearch_status = test_elasticsearch
   end

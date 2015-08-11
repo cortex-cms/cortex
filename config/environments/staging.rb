80fc9e9207dac434bef01a9d03d4a4ffc91ba335
toastercup <toastercup@gmail.com>
2015-08-11 16:57:01 -0400

    Update environment configs to support new Paperclip/S3 config requirements

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -33,9 +33,10 @@ Cortex::Application.configure do
     config.paperclip_defaults = {
       :storage => :s3,
       :s3_credentials => {
-        :bucket => ENV['S3_BUCKET_NAME'],
         :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
         :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'],
+        :s3_region => ENV['S3_REGION'],
+        :bucket => ENV['S3_BUCKET_NAME'],
         :url => ':s3_alias_url',
         :s3_host_alias => ENV['S3_HOST_ALIAS']
       }

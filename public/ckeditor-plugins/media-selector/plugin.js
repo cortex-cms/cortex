06bbeda2635a0975bd403c32366f8795ce0a9957
toastercup <toastercup@gmail.com>
2015-09-03 16:10:38 -0400

    Add Source Dialog plugin, and begin implementation of Video+Image+PDF media type insertion for in-page editing

--- public/ckeditor-plugins/media-selector/plugin.js
+++ public/ckeditor-plugins/media-selector/plugin.js
@@ -14,11 +14,27 @@
           global.media_select_defer.promise(global.media_select);
 
           global.media_select.done(function (media) {
-            var media_html = '<img src="' + media.url + '" data-media-id="' + media.id + '" alt="' + media.name + '"></img>';
-            editor.insertHtml(media_html);
-
+            editor.insertHtml(getMediaHtml(media));
             mediaLibraryModal.closeModal();
           });
+
+          var getMediaHtml = function(media) {
+            var media_html;
+
+            switch(media.content_type) {
+              case 'image':
+                media_html = '<img src="' + media.url + '" data-media-id="' + media.id + '" alt="' + media.name + '"></img>';
+                break;
+              case 'youtube':
+                media_html = '<iframe data-media-id="' + media.id + '" type="text/html" src="http://www.youtube.com/embed/' + media.video_id + '" frameborder="0" style="width: 100%; height: auto;" />';
+                break;
+              case 'pdf':
+                media_html = '<a href="' + media.url + '" data-media-id="' + media.id + '">' + media.name + '</a>';
+                break;
+            }
+
+            return media_html;
+          }
         }
       });
 

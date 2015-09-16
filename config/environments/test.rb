2aa001493b34080ff2e1438cdfa662511839c518
Alex Tharp <toastercup@gmail.com>
2015-09-16 16:16:06 -0400

    Remove TLS configuration from ActionMailer, switch to login auth

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -52,14 +52,13 @@ Cortex::Application.configure do
   config.action_mailer.default_url_options = {:host => ENV['HOST']}
   config.action_mailer.delivery_method = :test
   config.action_mailer.smtp_settings = {
-    :authentication => :plain,
+    :authentication => :login,
     :address => ENV['SMTP_ADDRESS'],
     :port => ENV['SMTP_PORT'],
     :domain => ENV['SMTP_SENDER_DOMAIN'],
     :user_name => ENV['SMTP_USERNAME'],
     :password => ENV['SMTP_PASSWORD'],
-    :enable_starttls_auto => ENV['SMTP_STARTTLS'],
-    :tls => ENV['SMTP_STARTTLS']
+    :enable_starttls_auto => ENV['SMTP_STARTTLS']
   }
 
 end

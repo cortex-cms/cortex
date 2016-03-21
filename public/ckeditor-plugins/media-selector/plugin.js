ce7808847def6ae127a939fe8a4ab253a5684f97
Alex Tharp <toastercup@gmail.com>
2016-03-21 14:42:21 -0500

    Restore CKEditor Media Selector Plugin logic, pending promise refactor/removal

--- public/ckeditor-plugins/media-selector/plugin.js
+++ public/ckeditor-plugins/media-selector/plugin.js
@@ -6,26 +6,35 @@
     init: function (editor) {
       editor.addCommand('insertMedia', {
         exec: function (editor) {
-          console.log(this);
+          var mediaLibraryModal = $('#media-library-modal');
+          mediaLibraryModal.openModal();
 
+          global.media_select = {};
+          global.media_select_defer = $.Deferred();
+          global.media_select_defer.promise(global.media_select);
 
+          global.media_select.done(function (media) {
+            editor.insertHtml(getMediaHtml(media));
+            mediaLibraryModal.closeModal();
+          });
 
+          var getMediaHtml = function(media) {
+            var media_html;
 
+            switch(media.content_type) {
+              case 'image':
+                media_html = '<img src="' + media.url + '" data-media-id="' + media.id + '" alt="' + media.name + '"></img>';
+                break;
+              case 'youtube':
+                media_html = '<iframe data-media-id="' + media.id + '" type="text/html" src="http://www.youtube.com/embed/' + media.video_id + '" frameborder="0" style="width: 100%; height: auto;" />';
+                break;
+              case 'pdf':
+                media_html = '<a href="' + media.url + '" data-media-id="' + media.id + '">' + media.name + '</a>';
+                break;
+            }
 
-
-
-
-
-
-
-
-
-
-
-          global.utility.currentEditor = editor;
-          global.utility.mediaLibraryModal = $('#media-library-modal');
-
-          global.utility.mediaLibraryModal.openModal();
+            return media_html;
+          }
         }
       });
 

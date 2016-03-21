c66ad547ed1a1c28668cb9c909cfa2e2321807cc
ElliottAYoung <elliott.a.young@gmail.com>
2016-03-21 10:47:34 -0500

    Get first test passing - test login

---
+++ spec/features/user_auth_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+describe "User Authentication", :type => :feature, :js => true do
+  describe 'Signing In' do
+    before :each do
+      tenant = Tenant.create!({name: "test"})
+      @user = User.create!({:email => 'user@example.com', :password => 'password12345!', tenant_id: tenant.id, firstname: "Alias", lastname: "Testname", admin: false})
+      
+    end
+
+    it "Signs the User In" do
+      visit '/#/login'
+      fill_in 'user_email', :with => @user.email
+      fill_in 'user_password', :with => @user.password
+      click_button 'Sign In'
+      expect(page).to have_content 'Home'
+    end
+  end
+end

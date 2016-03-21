b2df2d8af0bb70d2d33fa233567a70110dbe7cbd
ElliottAYoung <elliott.a.young@gmail.com>
2016-03-21 15:30:36 -0500

    refactor the tests

--- spec/features/main_cortex_interface_spec.rb
+++ spec/features/main_cortex_interface_spec.rb
@@ -1,65 +1,63 @@
 require 'rails_helper'
 
 describe "Main Cortex Interface", :type => :feature, :js => true do
+  let(:admin) { FactoryGirl.create :user, :admin }
+  let(:user)  { FactoryGirl.create :user  }
 
   context 'User is an Admin' do
-
-    before(:each) do
-      tenant = Tenant.create!({name: "test"})
-      @admin = User.create!({:email => 'user@example.com', :password => 'password12345!', tenant_id: tenant.id, firstname: "Alias", lastname: "Testname", admin: true})
-
-      visit '/#/login'
-      fill_in 'user_email', :with => @admin.email
-      fill_in 'user_password', :with => @admin.password
-      click_button 'Sign In'
-    end
-
-    describe 'Tenants' do
-      it 'should show Organizations as an option on the top bar' do
-        expect(page).to have_content 'Tenants'
+    context 'When Logged in' do
+      before(:each) do
+        visit '/#/login'
+        fill_in 'user_email', :with => admin.email
+        fill_in 'user_password', :with => admin.password
+        click_button 'Sign In'
       end
-    end
 
-    describe 'Webpages' do
-      it 'should show Webpages as an option on the top bar' do
-        expect(page).to have_content 'Webpages'
+      describe 'Tenants' do
+        it 'should show Organizations as an option on the top bar' do
+          expect(page).to have_content 'Tenants'
+        end
+      end
+
+      describe 'Webpages' do
+        it 'should show Webpages as an option on the top bar' do
+          expect(page).to have_content 'Webpages'
+        end
       end
     end
   end
 
   context 'User is not an Admin' do
-
-    before(:each) do
-      tenant = Tenant.create!({name: "test"})
-      @notadmin = User.create!({:email => 'user2@example.com', :password => 'password12345!', tenant_id: tenant.id, firstname: "Alias", lastname: "Testname", admin: false})
-
-      visit '/#/login'
-      fill_in 'user_email', :with => @notadmin.email
-      fill_in 'user_password', :with => @notadmin.password
-      click_button 'Sign In'
-    end
-
-    describe 'Tenants' do
-      it 'should show not Organizations as an option on the top bar' do
-        expect(page).to_not have_content 'Tenants'
+    context 'When Logged in' do
+      before(:each) do
+        visit '/#/login'
+        fill_in 'user_email', :with => user.email
+        fill_in 'user_password', :with => user.password
+        click_button 'Sign In'
       end
 
-      it 'should redirect back to previous page if navigated to' do
-        back_path_url = current_path
-        visit '/#/organizations/'
-        expect(current_path).to eq(back_path_url)
-      end
-    end
+      describe 'Tenants' do
+        it 'should show not Organizations as an option on the top bar' do
+          expect(page).to_not have_content 'Tenants'
+        end
 
-    describe 'Webpages' do
-      it 'should show not Webpages as an option on the top bar' do
-        expect(page).to_not have_content 'Webpages'
+        it 'should redirect back to previous page if navigated to' do
+          back_path_url = current_path
+          visit '/#/organizations/'
+          expect(current_path).to eq(back_path_url)
+        end
       end
 
-      it 'should redirect back to previous page if navigated to' do
-        back_path_url = current_path
-        visit '/#/webpages/'
-        expect(current_path).to eq(back_path_url)
+      describe 'Webpages' do
+        it 'should show not Webpages as an option on the top bar' do
+          expect(page).to_not have_content 'Webpages'
+        end
+
+        it 'should redirect back to previous page if navigated to' do
+          back_path_url = current_path
+          visit '/#/webpages/'
+          expect(current_path).to eq(back_path_url)
+        end
       end
     end
   end
--- spec/features/user_auth_spec.rb
+++ spec/features/user_auth_spec.rb
@@ -2,15 +2,12 @@ require 'rails_helper'
 
 describe "User Authentication", :type => :feature, :js => true do
   describe 'Signing In' do
-    before :each do
-      tenant = Tenant.create!({name: "test"})
-      @user = User.create!({:email => 'user@example.com', :password => 'password12345!', tenant_id: tenant.id, firstname: "Alias", lastname: "Testname", admin: false})
-    end
+    let(:user) { FactoryGirl.create :user }
 
     it "Signs the User In" do
       visit '/#/login'
-      fill_in 'user_email', :with => @user.email
-      fill_in 'user_password', :with => @user.password
+      fill_in 'user_email', :with => user.email
+      fill_in 'user_password', :with => user.password
       click_button 'Sign In'
       expect(page).to have_content 'Home'
     end

2d762b82056dd2d11b7ab797d985079da1fd367a
Alex Tharp <toastercup@gmail.com>
2016-03-22 05:04:27 -0500

    Extract and refactor Webpage cache buster logic into ActiveJob

--- app/api/v1/resources/webpages.rb
+++ app/api/v1/resources/webpages.rb
@@ -1,5 +1,4 @@
 require_relative '../helpers/resource_helper'
-require 'uri'
 
 module API
   module V1
@@ -72,14 +71,7 @@ module API
             }
 
             webpage.update!(update_params.to_hash)
-
-            # HTTP request to application to clear cache
-            root_domain_uri = URI.parse(webpage.url)
-            cache_buster_url = root_domain_uri.scheme + "://" + root_domain_uri.host + "/cache?path=" + URI.encode(root_domain_uri.path)
-            Thread.new {
-              Excon.get cache_buster_url
-            }
-
+            CacheBustWebpageJob.perform_later(webpage.url)
 
             present webpage, with: Entities::Webpage, full: true
           end

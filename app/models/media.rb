28304e70d770f2fbf5969b23bbe2b37b4ceeaaa5
Kurt Edelbrock <kurt.edelbrock@careerbuilder.com>
2016-03-22 14:05:19 -0400

    Removing spare comma

--- app/models/media.rb
+++ app/models/media.rb
@@ -28,7 +28,7 @@ class Media < ActiveRecord::Base
                     },
                     :processors => [:thumbnail, :paperclip_optimizer],
                     :preserve_files => 'true',
-                    :path => ':class/:attachment/:style-:id.:extension',
+                    :path => ':class/:attachment/:style-:id.:extension'
 
   validates_attachment :attachment,
                        :presence => true,

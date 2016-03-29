1276c0e0e26e19c5ef62efa1dbb6874bd7f45981
ElliottAYoung <elliott.a.young@gmail.com>
2016-03-29 11:34:38 -0500

    Only use v3 API for helper

--- app/helpers/youtube_helper.rb
+++ app/helpers/youtube_helper.rb
@@ -1,26 +1,17 @@
 module YoutubeHelper
   def self.fetch_info(video_id)
-    video = {}
-    video[:url] = "https://www.youtube.com/v/#{video_id}"
-    r = Excon.get("http://www.youtube.com/oembed?url=#{video[:url]}&format=json")
-
-    raise "Error while requesting Youtube data\nStatus: #{r.status}\nBody: #{r.body}" if r.status >= 300
-
-    data                        = JSON.parse(r.body)
-    video[:thumbnail]           = data["thumbnail_url"]
-    video[:title]               = data["title"]
-    video[:authors]             = data["author_name"]
-
-    ## The metadata for these fields ^^ is only accessible from the oembed JSON request
-    ## The metadata for these fields \/ is only accessible from the v3 API
-
     video = Yt::Video.new id:  video_id
+    
+    info = {}
+    info[:url] = "https://www.youtube.com/v/#{video_id}"
+    info[:thumbnail]           = video.thumbnail_url
+    info[:title]               = video.title
+    info[:authors]             = video.channel_name
+    info[:duration]            = video.duration
+    info[:description]         = video.description
+    info[:source_published_at] = DateTime.parse(video.published_at)
+    info[:source_updated_at]   = video[:source_published_at]
 
-    video[:duration]            = video.duration
-    video[:description]         = video.description
-    video[:source_published_at] = DateTime.parse(video.published_at)
-    video[:source_updated_at]   = video[:source_published_at]
-
-    video
+    info
   end
 end

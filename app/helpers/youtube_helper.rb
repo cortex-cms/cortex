da792294a08dcbea1478f8d9f8f80ad111f5b86c
ElliottAYoung <elliott.a.young@gmail.com>
2016-03-29 10:43:37 -0500

    Change requests to new v3 API

--- app/helpers/youtube_helper.rb
+++ app/helpers/youtube_helper.rb
@@ -1,18 +1,15 @@
 module YoutubeHelper
   def self.fetch_info(video_id)
     video = {}
-    # This process uses the deprecated YouTube Data API v2.
-    # The v2 API is not guaranteed to function after April 20th, 2015
-    # TODO: Switch to v3: https://developers.google.com/youtube/v3/docs/videos/list
-    r = Excon.get("https://gdata.youtube.com/feeds/api/videos/#{video_id}?v=2&alt=json")
+    video[:url] = "https://www.youtube.com/v/#{video_id}"
+    r = Excon.get("http://www.youtube.com/oembed?url=#{video[:url]}&format=json")
 
     raise "Error while requesting Youtube data\nStatus: #{r.status}\nBody: #{r.body}" if r.status >= 300
 
     data                        = JSON.parse(r.body)
-    video[:url]                 = "https://www.youtube.com/v/#{video_id}"
-    video[:thumbnail]           = "https://i1.ytimg.com/vi/#{video_id}/hqdefault.jpg"
-    video[:title]               = data['entry']['title']['$t']
-    video[:authors]             = data['entry']['author'].collect{ |a| a['name']['$t'] }
+    video[:thumbnail]           = data["thumbnail_url"]
+    video[:title]               = data["title"]
+    video[:authors]             = data["author_name"]
     video[:duration]            = data['entry']['media$group']['yt$duration']['seconds']
     video[:description]         = data['entry']['media$group']['media$description']['$t']
     video[:source_published_at] = DateTime.parse(data['entry']['published']['$t'])

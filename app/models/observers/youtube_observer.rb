f7dd8fa21cee9d7e883a4723077fed18984735fa
Alex Tharp <toastercup@gmail.com>
2016-03-30 14:21:12 -0500

    Add temporary workaround so Youtube Media will exist for ActiveJob

--- app/models/observers/youtube_observer.rb
+++ app/models/observers/youtube_observer.rb
@@ -1,5 +1,8 @@
 class YoutubeObserver < ActiveRecord::Observer
   def after_create(media)
-    YoutubeMediaJob.perform_later(media)
+    # Delay job by 5 seconds, as there appears to be a bug somewhere as it relates to STI+Observers+ActiveJob.
+    # The Media object mysteriously will not exist after_create..
+    # TODO: Remove this workaround.
+    YoutubeMediaJob.set(wait: 5.seconds).perform_later(media)
   end
 end

ec7a49bc9c986c5618c3f2d48a6b4cda44acfd1a
Alex Tharp <toastercup@gmail.com>
2016-04-21 13:12:49 -0500

    Critical: Fix non-admin users potentially being able to alter other users' data. Fix User specs, add Admin User spec

--- spec/api/v1/resources/users_spec.rb
+++ spec/api/v1/resources/users_spec.rb
@@ -2,9 +2,7 @@ require 'spec_helper'
 require 'api_v1_helper'
 
 describe SPEC_API::Resources::Users, :type => :request do
-
   describe 'GET /users/me' do
-
     let(:user) { create(:user) }
 
     it 'should get the current user if authorized' do
@@ -21,7 +19,8 @@ describe SPEC_API::Resources::Users, :type => :request do
   end
 
   describe 'PUT /users/:user_id' do
-    let(:user) { create(:user, password: "TestPassword") }
+    let(:user) { create(:user, password: 'TestPassword') }
+    let(:admin_user) { create(:user, :admin) }
     let(:other_user) { create(:user) }
 
     context 'as the target user' do
@@ -30,18 +29,18 @@ describe SPEC_API::Resources::Users, :type => :request do
       end
 
       it 'should reject if the current password is incorrect' do
-        put "/api/v1/users/#{user.id}", {current_password: "WrongPassword", password: "NewPassword", password_confirmation: "NewPassword"}
+        put "/api/v1/users/#{user.id}", {current_password: 'WrongPassword', password: 'NewPassword', password_confirmation: 'NewPassword'}
         expect(response.status).to eq(403)
       end
 
       it 'should reject if the passwords don\'t match' do
-        put "/api/v1/users/#{user.id}", {current_password: "TestPassword", password: "NewPassword", password_confirmation: "Different Password"}
+        put "/api/v1/users/#{user.id}", {current_password: 'TestPassword', password: 'NewPassword', password_confirmation: 'Different Password'}
         expect(response.status).to eq(422)
       end
 
       it 'should change the password when all is correct' do
-        put "/api/v1/users/#{user.id}", {current_password: "TestPassword", password: "NewPassword", password_confirmation: "NewPassword"}
-        expect(response.status).to eq(200)
+        put "/api/v1/users/#{user.id}", {current_password: 'TestPassword', password: 'NewPassword', password_confirmation: 'NewPassword'}
+        expect(response).to be_success
       end
     end
 
@@ -51,8 +50,19 @@ describe SPEC_API::Resources::Users, :type => :request do
       end
 
       it 'should reject in all cases' do
-        put "/api/v1/users/#{user.id}", {current_password: "TestPassword", password: "NewPassword", password_confirmation: "NewPassword"}
+        put "/api/v1/users/#{user.id}", {current_password: 'TestPassword', password: 'NewPassword', password_confirmation: 'NewPassword'}
         expect(response.status).to eq(403)
+      end
+    end
+
+    context 'as an admin' do
+      before :each do
+        login_as admin_user
+      end
+
+      it 'should succeed but not change password of other user' do
+        put "/api/v1/users/#{user.id}", {current_password: 'TestPassword', password: 'NewPassword', password_confirmation: 'NewPassword'}
+        expect(response).to be_success
       end
     end
   end

b0e02d9ba05ae800aa5765bbec998ecb43606089
ElliottAYoung <elliott.a.young@gmail.com>
2016-05-05 16:06:19 -0500

    Properly handle seo_keywords when submitted in controller

--- app/assets/javascripts/controllers/posts/edit.js
+++ app/assets/javascripts/controllers/posts/edit.js
@@ -28,6 +28,7 @@ angular.module('cortex.controllers.posts.edit', [
       $scope.data.post.primary_category_id = $scope.data.post.category_ids[0];
       $scope.data.post.industry_ids = [$scope.data.post.primary_industry_id];
       $scope.data.post.tag_list = $scope.data.post.tag_list.map(function(tag) { return tag.name; });
+      $scope.data.post.seo_keyword_list = $scope.data.post.seo_keyword_list.map(function(seo_keyword) { return seo_keyword.name; });
 
       if ($scope.data.authorIsUser) {
         $scope.data.post.custom_author = null;

c4d999a704a29020e16c20d9b4a60435ea5dd317
ElliottAYoung <elliott.a.young@gmail.com>
2016-05-05 17:42:56 -0500

    Add acts_as_taggable to webpage seo_keywrod

--- app/models/webpage.rb
+++ app/models/webpage.rb
@@ -3,6 +3,7 @@ class Webpage < ActiveRecord::Base
   include SearchableWebpage
 
   acts_as_paranoid
+  acts_as_taggable_on :seo_keywords
 
   belongs_to :user
   has_many :snippets, inverse_of: :webpage

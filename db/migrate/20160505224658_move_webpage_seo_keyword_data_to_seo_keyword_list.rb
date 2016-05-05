b70cc1c911e3f103ded562a33fa72d9ae94094c2
ElliottAYoung <elliott.a.young@gmail.com>
2016-05-05 18:07:19 -0500

    Add comments to the migrations explaining them

--- db/migrate/20160505224658_move_webpage_seo_keyword_data_to_seo_keyword_list.rb
+++ db/migrate/20160505224658_move_webpage_seo_keyword_data_to_seo_keyword_list.rb
@@ -1,5 +1,10 @@
+# Pretty straight forward - is a block that moves all the string data from
+# seo_keywords_old (recently renamed) to the acts_as_taggable list using the
+# #add method. This is done to preserve existing data before this refactor was
+# made.
+
 class MoveWebpageSeoKeywordDataToSeoKeywordList < ActiveRecord::Migration
-  def up
+  def change
     Webpage.all.each do |w|
       unless w.seo_keywords_old.nil?
         keywords = w.seo_keywords_old.split(" ")
@@ -7,9 +12,5 @@ class MoveWebpageSeoKeywordDataToSeoKeywordList < ActiveRecord::Migration
         w.save
       end
     end
-  end
-
-  def down
-    #Not Yet Implemented
   end
 end

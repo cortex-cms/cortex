49778b10374b1773b8fbbdb6d9dc3967b4ffde1a
Alex Tharp <toastercup@gmail.com>
2016-05-30 22:26:47 -0400

    Initial login form design: dual pane login-and-news layout, dual login options

--- app/controllers/authentication/sessions_controller.rb
+++ app/controllers/authentication/sessions_controller.rb
@@ -1,7 +1,10 @@
 class Authentication::SessionsController < Devise::SessionsController
   def new
     super do |user|
-      puts "I'm invoked!"
+      params = Hashr.new(page: 1, per_page: 10)
+      tenant = Tenant.find_by_id(Cortex.config.cortex.news_feed.tenant)
+
+      @news_feed_posts = ::GetPosts.call(params: params, tenant: tenant, published: true).posts
     end
   end
 end
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,5 +9,8 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
+Rails.application.config.assets.precompile += %w( authentication.scss )
+Rails.application.config.assets.precompile += %w( authentication.js )
+
 Rails.application.config.assets.precompile += %w( legacy_application.scss )
 Rails.application.config.assets.precompile += %w( legacy_application.js )

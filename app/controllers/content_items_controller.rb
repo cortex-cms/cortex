c9c009aea4c4be762ba6d7f83f38f60033a20f48
Eryan Cobham <eryan@devmynd.com>
2016-06-20 18:03:34 -0500

    WIP - Fixing new form

--- app/controllers/content_items_controller.rb
+++ app/controllers/content_items_controller.rb
@@ -2,17 +2,17 @@ class ContentItemsController < ApplicationController
 
   def new
     @content_type = ContentType.find(params[:content_type_id])
-    @content_item = ContentItem.new
+    @content_item = @content_type.content_items.new
   end
 
   def create
     binding.pry
-    content_item = ContentItem.new(content_item_params)
-    if content_item.save
-      params[:field_item][:data].each do |field_items|
-      end
-      redirect_to content_type_content_items_path(content_type_id: params[:content_type_id])
+    @content_item = ContentItem.new(content_item_params)
+    if @content_item.save
+      redirect_to content_types_path(content_type_id: params[:content_type_id])
     else
+      @content_type = ContentType.find(params[:content_type_id])
+      render :new
     end
   end
 
@@ -20,17 +20,16 @@ class ContentItemsController < ApplicationController
 
   def content_item_params
     params.require(:content_item).permit(
-    :author,
-    :creator,
-    :content_type_id,
-    field_items_attributes: field_item_params
+      :author_id,
+      :creator_id,
+      :content_type_id,
+      field_item: field_item_params
     )
   end
 
   def field_item_params
     [
       :field_id,
-      :content_item_id,
       :data
     ]
   end

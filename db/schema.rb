748ecb5ee2644380c3d92ed13799a6407fa190c0
ElliottAYoung <elliott.a.young@gmail.com>
2016-06-29 14:04:38 -0500

    Reforge Permissions and RolePermissions

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160629184458) do
+ActiveRecord::Schema.define(version: 20160629190226) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -258,7 +258,6 @@ ActiveRecord::Schema.define(version: 20160629184458) do
   create_table "permissions", force: :cascade do |t|
     t.string   "name"
     t.string   "resource_type"
-    t.integer  "resource_id"
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
   end

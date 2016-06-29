ad92a7b3455796468af71603b2e866048d05f429
ElliottAYoung <elliott.a.young@gmail.com>
2016-06-29 16:30:02 -0500

    Write Content Type Policy Specs

--- spec/policies/content_type_policy_spec.rb
+++ spec/policies/content_type_policy_spec.rb
@@ -19,4 +19,18 @@ describe ContentTypePolicy do
     it { should permit(:edit) }
     it { should permit(:destroy) }
   end
-end 
+
+  context "for a non admin user" do
+    subject { ContentTypePolicy.new(user, content_type) }
+    let(:content_type) { create(:content_type) }
+    let(:user) { create(:user) }
+
+    it { should_not permit(:show) }
+    it { should_not permit(:index) }
+    it { should_not permit(:create) }
+    it { should_not permit(:new) }
+    it { should_not permit(:update) }
+    it { should_not permit(:edit) }
+    it { should_not permit(:destroy) }
+  end
+end

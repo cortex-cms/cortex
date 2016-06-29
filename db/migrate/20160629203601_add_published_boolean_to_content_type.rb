de4162879dc6ac165a535e40773208eb0535bf8c
ElliottAYoung <elliott.a.young@gmail.com>
2016-06-29 16:25:00 -0500

    Add Default of false to Published Boolean

--- db/migrate/20160629203601_add_published_boolean_to_content_type.rb
+++ db/migrate/20160629203601_add_published_boolean_to_content_type.rb
@@ -1,6 +1,6 @@
 class AddPublishedBooleanToContentType < ActiveRecord::Migration
   def change
-    add_column :content_types, :is_published, :boolean
+    add_column :content_types, :is_published, :boolean, default: false
     add_column :permissions, :resource_id, :integer
   end
 end

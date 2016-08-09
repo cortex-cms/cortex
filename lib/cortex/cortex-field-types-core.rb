031c4f582069fa993245449fd2fa004837dde968
ElliottAYoung <elliott.a.young@gmail.com>
2016-08-09 10:51:08 -0500

    Do a general require of everything in the Core directory

--- lib/cortex/cortex-field-types-core.rb
+++ lib/cortex/cortex-field-types-core.rb
@@ -1,14 +1,1 @@
-require 'cortex/field_types/core/text/text_field_type'
-require 'cortex/field_types/core/text/text_cell'
-
-require 'cortex/field_types/core/boolean/boolean_field_type'
-require 'cortex/field_types/core/boolean/boolean_cell'
-
-require 'cortex/field_types/core/tree/tree_field_type'
-require 'cortex/field_types/core/tree/tree_cell'
-
-require 'cortex/field_types/core/date_time/date_time_field_type'
-require 'cortex/field_types/core/date_time/date_time_cell'
-
-require 'cortex/field_types/core/user/user_field_type'
-require 'cortex/field_types/core/user/user_cell'
+require 'cortex/field_types/core/*'

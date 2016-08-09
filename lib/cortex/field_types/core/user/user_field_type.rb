ab4d616e73be4ea84bb2647f9fa9ab9bb3bab5ad
ElliottAYoung <elliott.a.young@gmail.com>
2016-08-09 10:02:22 -0500

    Create general User Field Type

---
+++ lib/cortex/field_types/core/user/user_field_type.rb
@@ -0,0 +1,77 @@
+class UserFieldType < FieldType
+  VALIDATION_TYPES = {
+    presence: :valid_presence_validation?
+  }.freeze
+
+  attr_accessor :id, :text, :field_name
+  attr_reader :validations, :metadata
+
+  validates :id, presence: true, if: :validate_presence?
+  validates :valid_user_id
+
+  def validations=(validations_hash)
+    @validations = validations_hash.deep_symbolize_keys
+  end
+
+  def data=(data_hash)
+    @text = data_hash.deep_symbolize_keys[:text]
+  end
+
+  def metadata=(metadata_hash)
+    @metadata = metadata_hash.deep_symbolize_keys
+  end
+
+  def acceptable_validations?
+    valid_types? && valid_options?
+  end
+
+  def field_item_as_indexed_json_for_field_type(field_item, options = {})
+    json = {}
+    json[mapping_field_name] = field_item.data['id']
+    json
+  end
+
+  def mapping
+    {name: mapping_field_name, type: :string, analyzer: :snowball}
+  end
+
+  def valid_user_id
+    begin
+      User.find(id)
+      true
+    rescue ActiveRecord::RecordNotFound
+      errors.add(:user_id, "must be for a valid User")
+      false
+    end
+  end
+
+  private
+
+  def mapping_field_name
+    "#{field_name.downcase}_user"
+  end
+
+  def valid_types?
+    validations.all? do |type, options|
+      VALIDATION_TYPES.include?(type)
+    end
+  end
+
+  def valid_options?
+    validations.all? do |type, options|
+      self.send(VALIDATION_TYPES[type])
+    end
+  end
+
+  def valid_presence_validation?
+    @validations.key? :presence
+  end
+
+  def text_present
+    errors.add(:text, "must be present") if @text.empty?
+  end
+
+  def validate_presence?
+    @validations.key? :presence
+  end
+end

db9f2b943ec5397bc2ddb582b01b2eb98d31e856
ElliottAYoung <elliott.a.young@gmail.com>
2016-08-09 10:12:45 -0500

    Able to create User Relation Field Types

--- lib/cortex/field_types/core/user/user_field_type.rb
+++ lib/cortex/field_types/core/user/user_field_type.rb
@@ -3,18 +3,18 @@ class UserFieldType < FieldType
     presence: :valid_presence_validation?
   }.freeze
 
-  attr_accessor :id, :text, :field_name
+  attr_accessor :data, :id, :field_name
   attr_reader :validations, :metadata
 
   validates :id, presence: true, if: :validate_presence?
-  validates :valid_user_id
+  validate :valid_user_id?
 
   def validations=(validations_hash)
     @validations = validations_hash.deep_symbolize_keys
   end
 
   def data=(data_hash)
-    @text = data_hash.deep_symbolize_keys[:text]
+    @values = data_hash.deep_symbolize_keys[:id]
   end
 
   def metadata=(metadata_hash)
@@ -27,7 +27,7 @@ class UserFieldType < FieldType
 
   def field_item_as_indexed_json_for_field_type(field_item, options = {})
     json = {}
-    json[mapping_field_name] = field_item.data['id']
+    json[mapping_field_name] = field_item.data['user']
     json
   end
 
@@ -35,20 +35,20 @@ class UserFieldType < FieldType
     {name: mapping_field_name, type: :string, analyzer: :snowball}
   end
 
-  def valid_user_id
-    begin
-      User.find(id)
-      true
-    rescue ActiveRecord::RecordNotFound
-      errors.add(:user_id, "must be for a valid User")
-      false
-    end
-  end
-
   private
 
   def mapping_field_name
-    "#{field_name.downcase}_user"
+    "#{field_name.downcase.gsub(' ', '_')}_user"
+  end
+
+  def valid_user_id?
+    begin
+      User.find(id)
+      true
+    rescue ArgumentError
+      errors.add(:user_id, 'must be for a valid User')
+      false
+    end
   end
 
   def valid_types?
@@ -65,10 +65,6 @@ class UserFieldType < FieldType
 
   def valid_presence_validation?
     @validations.key? :presence
-  end
-
-  def text_present
-    errors.add(:text, "must be present") if @text.empty?
   end
 
   def validate_presence?

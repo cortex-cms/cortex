c3171b5da9b76a254b704188750ab8cf429255de
ElliottAYoung <elliott.a.young@gmail.com>
2016-08-10 14:44:22 -0500

    Refactor tag updating into service layer

--- app/services/content_item_service.rb
+++ app/services/content_item_service.rb
@@ -19,6 +19,13 @@ class ContentItemService < CortexService
     end
   end
 
+  def self.update_tags(content_item, tag_data_array)
+    tag_list_name = "#{tag_data_array[0].downcase.gsub(' ', '_').singularize}_list="
+    tag_array = tag_data_array[1]
+
+    content_item.send(tag_list_name, tag_array)
+  end
+
   private
 
   def transact_and_refresh

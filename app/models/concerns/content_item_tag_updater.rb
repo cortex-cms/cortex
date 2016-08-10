1def310cb77c6f220c27849b744b153770da1410
ElliottAYoung <elliott.a.young@gmail.com>
2016-08-10 14:44:06 -0500

    Delete Tag Concern

--- app/models/concerns/content_item_tag_updater.rb
+++
@@ -1,8 +0,0 @@
-module ContentItemTagUpdater
-  def self.update_tags(content_item, tag_data_array)
-    tag_list_name = "#{tag_data_array[0].downcase.gsub(' ', '_').singularize}_list="
-    tag_array = tag_data_array[1]
-
-    content_item.send(tag_list_name, tag_array)
-  end
-end

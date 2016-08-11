9e6c74a4186893456f8e49f1b318dad6b01547fa
ElliottAYoung <elliott.a.young@gmail.com>
2016-08-11 10:04:07 -0500

    Push for Review - Tag works, User and DateTime dont

---
+++ lib/cortex/field_types/core/tag/tag_cell.rb
@@ -0,0 +1,15 @@
+module Cortex
+  module FieldTypes
+    module Core
+      module Tag
+        class TagCell < FieldCell
+
+          def tag_picker
+            render
+          end
+
+        end
+      end
+    end
+  end
+end

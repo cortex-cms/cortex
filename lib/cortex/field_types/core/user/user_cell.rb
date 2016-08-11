ece5376087c6a7166cdecff3ba316455db3406b5
ElliottAYoung <elliott.a.young@gmail.com>
2016-08-11 14:38:37 -0500

    User Cell Dropdown works as expected

--- lib/cortex/field_types/core/user/user_cell.rb
+++ lib/cortex/field_types/core/user/user_cell.rb
@@ -8,6 +8,24 @@ module Cortex
             render
           end
 
+          private
+
+          def value
+            if data
+              data['user_id'] || @options[:default_value]
+            else
+              @options[:default_value]
+            end
+          end
+
+          def render_select_options
+            options = ""
+            @options[:user_data].each do |user_info|
+              options += "<option value='#{user_info[1]}'>#{user_info[0]}</option>"
+            end
+            options.html_safe
+          end
+
         end
       end
     end

4f099ace57316e02c474c98a6115aad5f9fe8428
ElliottAYoung <elliott.a.young@gmail.com>
2016-08-15 09:39:41 -0500

    General form set up with jQuery Datepicker

--- lib/cortex/field_types/core/date_time/date_time_cell.rb
+++ lib/cortex/field_types/core/date_time/date_time_cell.rb
@@ -8,6 +8,20 @@ module Cortex
             render
           end
 
+          private
+
+          def value
+            if data
+              data['timestamp'] || @options[:default_value]
+            else
+              @options[:default_value]
+            end
+          end
+
+          def render_datepicker
+            @options[:form].text_field 'data[timestamp]', value: value, placeholder: @options[:placeholder], class: 'datepicker'
+          end
+
         end
       end
     end

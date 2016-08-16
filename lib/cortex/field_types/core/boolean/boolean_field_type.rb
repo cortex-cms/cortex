34d49fb714489e96a8145a8f742129fcd97f1f79
ElliottAYoung <elliott.a.young@gmail.com>
2016-08-16 15:17:29 -0500

    Remove validations from boolean

--- lib/cortex/field_types/core/boolean/boolean_field_type.rb
+++ lib/cortex/field_types/core/boolean/boolean_field_type.rb
@@ -1,17 +1,8 @@
 class BooleanFieldType < FieldType
-  VALIDATION_TYPES = {
-    presence: :valid_presence_validation?
-  }.freeze
-
   attr_accessor :data, :value, :field_name
-  attr_reader :validations, :metadata
+  attr_reader :metadata
 
-  validates :value, presence: true, if: :validate_presence?
   validate :value_is_allowed?
-
-  def validations=(validations_hash)
-    @validations = validations_hash.deep_symbolize_keys
-  end
 
   def data=(data_hash)
     @value = data_hash.deep_symbolize_keys[:value]
@@ -21,10 +12,6 @@ class BooleanFieldType < FieldType
     @metadata = metadata_hash.deep_symbolize_keys
   end
 
-  def acceptable_validations?
-    valid_types? && valid_options?
-  end
-
   def field_item_as_indexed_json_for_field_type(field_item, options = {})
     json = {}
     json[mapping_field_name] = field_item.data['boolean']
@@ -32,7 +19,7 @@ class BooleanFieldType < FieldType
   end
 
   def mapping
-    {name: mapping_field_name, type: :string, analyzer: :snowball}
+    { name: mapping_field_name, type: :boolean }
   end
 
   private
@@ -48,25 +35,5 @@ class BooleanFieldType < FieldType
       errors.add(:value, "must be True or False")
       false
     end
-  end
-
-  def valid_types?
-    validations.all? do |type, options|
-      VALIDATION_TYPES.include?(type)
-    end
-  end
-
-  def valid_options?
-    validations.all? do |type, options|
-      self.send(VALIDATION_TYPES[type])
-    end
-  end
-
-  def valid_presence_validation?
-    @validations.key? :presence
-  end
-
-  def validate_presence?
-    @validations.key? :presence
   end
 end

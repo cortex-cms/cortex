e1d149f45c8e7acfd58168f9665c362c30d8d60f
ElliottAYoung <elliott.a.young@gmail.com>
2016-08-25 14:27:03 -0500

    Deep Symbolize all keys for consistency

--- app/services/index_decorator_service.rb
+++ app/services/index_decorator_service.rb
@@ -2,6 +2,6 @@ class IndexDecoratorService < CortexService
   attribute :content_type, ContentType
 
   def data
-    @content_type.index_decorator.data
+    @content_type.index_decorator.data.deep_symbolize_keys
   end
 end
--- app/services/wizard_decorator_service.rb
+++ app/services/wizard_decorator_service.rb
@@ -2,6 +2,6 @@ class WizardDecoratorService < CortexService
   attribute :content_item, ContentItem
 
   def data
-    @content_item.content_type.wizard_decorator.data
+    @content_item.content_type.wizard_decorator.data.deep_symbolize_keys
   end
 end

e14e5b6306187472067a8b5d86a0454e4ec902d2
ElliottAYoung <elliott.a.young@gmail.com>
2016-08-25 15:30:23 -0500

    Change Edit Button to Pencil Icon

--- app/cells/index/content_item_cell.rb
+++ app/cells/index/content_item_cell.rb
@@ -14,7 +14,7 @@ module Index
       content_type = @options[:content_item].content_type
       content_item = @options[:content_item]
 
-      link_to "Edit #{content_item.id}", edit_content_type_content_item_path(content_type.id, content_item.id)
+      link_to "<i class='material-icons'>create</i>".html_safe, edit_content_type_content_item_path(content_type.id, content_item.id), class: 'mdl-button mdl-js-button mdl-button--icon'
     end
 
     def render_column(field)

d65e6a85f5e7d19df604bfccce9ff559bf519271
ElliottAYoung <elliott.a.young@gmail.com>
2016-08-25 14:14:44 -0500

    Add comments explaining faked methods

--- app/models/content_item.rb
+++ app/models/content_item.rb
@@ -20,6 +20,11 @@ class ContentItem < ActiveRecord::Base
     taggable_on_array = Field.select { |field| field.field_type_instance.is_a?(TagFieldType) }.map { |field_item| field_item.name.parameterize('_') }
   end
 
+  # The following two methods (#author_image and #publish_state) are currently faked
+  # author_image is faked pending being able to reference the specific User object (ex:
+  # content_item.author.user_image)
+  # publish_state is faked pending the story revolving around being able to publish ContentItems
+
   def author_image
     "<img src='https://robohash.org/#{rand(1000..10000)}.png' height='100' width='100'/>".html_safe
   end

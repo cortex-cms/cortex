6e5f83e3869d0d1cb78434ebcf9c0f892fff5423
Alex Tharp <toastercup@gmail.com>
2016-09-06 17:44:52 -0500

    Remove Tree concern, explicitly require Core FieldTypes due to eager-loading issues

--- lib/cortex/cortex-field-types-core.rb
+++ lib/cortex/cortex-field-types-core.rb
@@ -1,10 +1,17 @@
-
-require 'cortex/field_types/core/*'
-require 'cortex/field_types/core/text/text_field_type'
-require 'cortex/field_types/core/text/text_cell'
-
 require 'cortex/field_types/core/boolean/boolean_field_type'
 require 'cortex/field_types/core/boolean/boolean_cell'
 
+require 'cortex/field_types/core/date_time/date_time_field_type'
+require 'cortex/field_types/core/date_time/date_time_cell'
+
+require 'cortex/field_types/core/tag/tag_field_type'
+require 'cortex/field_types/core/tag/tag_cell'
+
+require 'cortex/field_types/core/text/text_field_type'
+require 'cortex/field_types/core/text/text_cell'
+
 require 'cortex/field_types/core/tree/tree_field_type'
 require 'cortex/field_types/core/tree/tree_cell'
+
+require 'cortex/field_types/core/user/user_field_type'
+require 'cortex/field_types/core/user/user_cell'

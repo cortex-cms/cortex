ae85760d91b858f194ed989e126ff3ea0a6698bd
ElliottAYoung <elliott.a.young@gmail.com>
2016-09-06 14:40:33 -0500

    Greatly Modify Tree Cell

--- lib/cortex/field_types/core/tree/tree_cell.rb
+++ lib/cortex/field_types/core/tree/tree_cell.rb
@@ -3,27 +3,17 @@ module Cortex
     module Core
       module Tree
         class TreeCell < FieldCell
-          def tree
+          def checkboxes
+            render
+          end
+
+          def dropdown
             render
           end
 
           private
 
-          def form
-            @options[:form]
-          end
-
-          def nodes
-            model.data.values_at([:category])
-          end
-
-          def categories
-            CategoryTree.build(options[:tree]).values_at([:category, :category_id])
-          end
-
-          def index
-            options[:index]
-          end
+          
 
         end
       end

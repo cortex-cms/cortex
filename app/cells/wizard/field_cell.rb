f995acf67d935069cf419b46a8c5bcd46d4ddba1
ElliottAYoung <elliott.a.young@gmail.com>
2016-09-07 13:42:46 -0500

    Add multi format support (choice of checkbox vs dropdown)

--- app/cells/wizard/field_cell.rb
+++ app/cells/wizard/field_cell.rb
@@ -3,12 +3,17 @@ module Wizard
     property :id
     property :label
     property :input
+    property :display_format
 
     def show
       render
     end
 
     private
+
+    def check_format
+      display_format
+    end
 
     def field_item
       context[:content_item].field_items.select { |field_item| field_item.field_id == id }[0]

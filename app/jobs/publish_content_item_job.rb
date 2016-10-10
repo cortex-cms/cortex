e8d9b09e8ccbbdf8279882baf5a4c9ca5040ea79
ElliottAYoung <elliott.a.young@gmail.com>
2016-10-10 10:27:11 -0500

    Working ActiveJob to publish Scheduled Items

--- app/jobs/publish_content_item_job.rb
+++ app/jobs/publish_content_item_job.rb
@@ -1,5 +1,8 @@
 class PublishContentItemJob < ActiveJob::Base
-  def perform(content_item)
+  queue_as :default
+
+  def perform(content_item_id)
+    content_item = ContentItem.find(content_item_id)
     content_item.publish!
   end
 end

6ffbc2a2984ff71144420fe00e2f8ce815af302f
ElliottAYoung <elliott.a.young@gmail.com>
2016-10-12 10:55:37 -0500

    Mark Custom Content Cortex Tests as Pending - rather than comment out

--- spec/models/content_type_spec.rb
+++ spec/models/content_type_spec.rb
@@ -1,16 +1,16 @@
-# require 'spec_helper'
-#
-# RSpec.describe ContentType, type: :model do
-#   subject { build(:content_type) }
-#
-#   context "validations" do
-#     it { is_expected.to validate_presence_of(:name) }
-#     it { is_expected.to validate_presence_of(:creator) }
-#   end
-#
-#   context "associations" do
-#     it { is_expected.to belong_to(:creator).class_name("User") }
-#     it { is_expected.to have_many(:fields) }
-#     it { is_expected.to have_many(:content_items) }
-#   end
-# end
+require 'spec_helper'
+
+RSpec.describe ContentType, type: :model do
+  subject { build(:content_type) }
+
+  context "validations" do
+    xit { is_expected.to validate_presence_of(:name) }
+    xit { is_expected.to validate_presence_of(:creator) }
+  end
+
+  context "associations" do
+    xit { is_expected.to belong_to(:creator).class_name("User") }
+    xit { is_expected.to have_many(:fields) }
+    xit { is_expected.to have_many(:content_items) }
+  end
+end
--- spec/policies/content_type_policy_spec.rb
+++ spec/policies/content_type_policy_spec.rb
@@ -1,55 +1,55 @@
-# require 'spec_helper'
-#
-# describe ContentTypePolicy do
-#   context "for an superadmin user" do
-#     subject { ContentTypePolicy.new(admin, content_type) }
-#     let(:content_type) { create(:content_type) }
-#     let(:superadmin) { create(:user) }
-#     let(:role) { Role.create(name: "superadmin") }
-#
-#     before do
-#       superadmin.roles << role
-#     end
-#
-#     it { should permit(:show) }
-#     it { should permit(:index) }
-#     it { should permit(:create) }
-#     it { should permit(:new) }
-#     it { should permit(:update) }
-#     it { should permit(:edit) }
-#     it { should permit(:destroy) }
-#   end
-#
-#   context "for an admin user" do
-#     subject { ContentTypePolicy.new(admin, content_type) }
-#     let(:content_type) { create(:content_type) }
-#     let(:admin) { create(:user) }
-#     let(:role) { Role.create(name: "admin") }
-#
-#     before do
-#       admin.roles << role
-#     end
-#
-#     it { should permit(:show) }
-#     it { should permit(:index) }
-#     it { should permit(:create) }
-#     it { should permit(:new) }
-#     it { should permit(:update) }
-#     it { should permit(:edit) }
-#     it { should permit(:destroy) }
-#   end
-#
-#   context "for a non admin user" do
-#     subject { ContentTypePolicy.new(user, content_type) }
-#     let(:content_type) { create(:content_type) }
-#     let(:user) { create(:user) }
-#
-#     it { should_not permit(:show) }
-#     it { should_not permit(:index) }
-#     it { should_not permit(:create) }
-#     it { should_not permit(:new) }
-#     it { should_not permit(:update) }
-#     it { should_not permit(:edit) }
-#     it { should_not permit(:destroy) }
-#   end
-# end
+require 'spec_helper'
+
+describe ContentTypePolicy do
+  context "for an superadmin user" do
+    subject { ContentTypePolicy.new(admin, content_type) }
+    let(:content_type) { create(:content_type) }
+    let(:superadmin) { create(:user) }
+    let(:role) { Role.create(name: "superadmin") }
+
+    before do
+      superadmin.roles << role
+    end
+
+    xit { should permit(:show) }
+    xit { should permit(:index) }
+    xit { should permit(:create) }
+    xit { should permit(:new) }
+    xit { should permit(:update) }
+    xit { should permit(:edit) }
+    xit { should permit(:destroy) }
+  end
+
+  context "for an admin user" do
+    subject { ContentTypePolicy.new(admin, content_type) }
+    let(:content_type) { create(:content_type) }
+    let(:admin) { create(:user) }
+    let(:role) { Role.create(name: "admin") }
+
+    before do
+      admin.roles << role
+    end
+
+    xit { should permit(:show) }
+    xit { should permit(:index) }
+    xit { should permit(:create) }
+    xit { should permit(:new) }
+    xit { should permit(:update) }
+    xit { should permit(:edit) }
+    xit { should permit(:destroy) }
+  end
+
+  context "for a non admin user" do
+    subject { ContentTypePolicy.new(user, content_type) }
+    let(:content_type) { create(:content_type) }
+    let(:user) { create(:user) }
+
+    xit { should_not permit(:show) }
+    xit { should_not permit(:index) }
+    xit { should_not permit(:create) }
+    xit { should_not permit(:new) }
+    xit { should_not permit(:update) }
+    xit { should_not permit(:edit) }
+    xit { should_not permit(:destroy) }
+  end
+end

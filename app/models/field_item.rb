c38240f6ac2a5c071c5ae67384171c4a3e0c5362
Alex Tharp <toastercup@gmail.com>
2016-10-18 13:57:08 -0500

    FieldItem model should have non-AssetFieldType-specific comments

--- app/models/field_item.rb
+++ app/models/field_item.rb
@@ -8,7 +8,7 @@ class FieldItem < ActiveRecord::Base
   validate :field_item_content_is_valid, if: :field_is_present
 
   def data=(data_hash)
-    # Reset @field_type_instance so that Paperclip data can be re-generated every time @data is set, not just on init
+    # Reset @field_type_instance so that massaged data can be re-generated every time @data is set, not just on init
     @field_type_instance = nil
     super(field_type_instance(data_hash).data || data_hash)
   end
@@ -17,7 +17,7 @@ class FieldItem < ActiveRecord::Base
 
   def field_type_instance(data_hash = nil)
     field_type_class = FieldType.get_subtype_constant(field.field_type)
-    # data_before_typecast will give us a non-mutilated hash with Tempfile intact, just in case validations get called first
+    # data_before_typecast will give us a non-mutilated hash with Objects intact, just in case validations get called first
     @field_type_instance ||= field_type_class.new(data: data_hash || data_before_type_cast, validations: field.validations)
     @field_type_instance if @field_type_instance.save!
   end

14e6d76779c7b29bb58bf6e74b47416730b5960f
ElliottAYoung <elliott.a.young@gmail.com>
2016-10-20 11:38:25 -0500

    Add Logic for Cells on the Index - because why not

--- app/cells/index/content_item_cell.rb
+++ app/cells/index/content_item_cell.rb
@@ -15,6 +15,8 @@ module Index
         @options[:content_item].field_items.find { |fi| fi.field_id == field[:id] }.data.values[0]
       elsif field.has_key?(:method)
         @options[:content_item].send(field[:method])
+      elsif field.has_key?(:cell)
+        cell(field[:cell][:class_name]).(field[:cell][:render_method])
       else
         ""
       end

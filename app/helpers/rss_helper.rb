67409004ffc1df2bcde2d44a7417323390d44af6
ElliottAYoung <elliott.a.young@gmail.com>
2016-10-31 11:33:27 -0500

    Handle the three decorator use cases

--- app/helpers/rss_helper.rb
+++ app/helpers/rss_helper.rb
@@ -5,16 +5,25 @@ module RssHelper
 
   def field_value(content_item, field_id)
     field = content_item.field_items.find_by_field_id(field_id)
-    truncate(field.data.values.join("")) unless field.nil?
+    field.data.values.join("") unless field.nil?
   end
 
-  private
+  def plugin_value(content_item, value_hash)
+    field = content_item.field_items.find_by_field_id(value_hash['data']['field_id'])
+    cell(value_hash['class_name'], field, display: value_hash['display'], config: value_hash['config']).(value_hash['render_method'])
+  end
 
-  def truncate(str)
-    if @config['summarize']
-      str.length > 50 ? str.slice(0..50) + "..." : str
-    else
-      str
+  def method_value(content_item, method)
+    content_item.send(method).to_s
+  end
+
+  def parse_data(key, value_hash, content_item)
+    if value_hash.keys.include?("field")
+      field_value(content_item, value_hash["field"])
+    elsif value_hash.include?("plugin")
+      plugin_value(content_item, value_hash["plugin"])
+    elsif value_hash.include?("method")
+      method_value(content_item, value_hash["method"])
     end
   end
 end

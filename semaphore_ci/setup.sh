81fe4cce4b5c3b8378a90089b03f848f03bcd58e
Alex Tharp <toastercup@gmail.com>
2016-11-14 19:03:23 -0600

    Upgrade Elasticsearch, remove unnecessary extensions and config options, ensure seeding doesn't happen twice, add ONET and Custom Content seeding, re-arrange bower setup, install bower devDependencies for test running, tweak sleep time

--- semaphore_ci/setup.sh
+++ semaphore_ci/setup.sh
@@ -10,31 +10,26 @@ bundle config build.nokogiri --use-system-libraries
 echo "bundle install --without production staging development tasks"
 bundle install --without production staging development tasks
 
+echo "bundle exec rake bower:install:development"
+bundle exec rake bower:install:development
+
 echo "ES Install"
 sudo service elasticsearch stop
-if ! [ -e .semaphore-cache/elasticsearch-2.3.1.deb ]; then (cd .semaphore-cache; curl -OL https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-2.3.1.deb); fi
-sudo dpkg -i --force-confnew .semaphore-cache/elasticsearch-2.3.1.deb
-
-sudo /usr/share/elasticsearch/bin/plugin install elasticsearch/elasticsearch-mapper-attachments/3.1.1
-echo "script.engine.groovy.inline.aggs: on" | sudo tee --append /etc/elasticsearch/elasticsearch.yml
-
-mkdir elasticsearch
+if ! [ -e .semaphore-cache/elasticsearch-2.4.1.deb ]; then (cd .semaphore-cache; curl -OL https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-2.4.1.deb); fi
+sudo dpkg -i --force-confnew .semaphore-cache/elasticsearch-2.4.1.deb
 sudo service elasticsearch start
 
-echo "sleep 10"
-sleep 10
+echo "sleep 5"
+sleep 5
 
 echo "ES Version Check"
 curl -XGET 'http://localhost:9200'
 
-echo "bundle exec rake db:setup"
-bundle exec rake db:setup
+echo "bundle exec rake db:drop db:create db:migrate db:seed"
+bundle exec rake db:drop db:create db:migrate db:seed
 
-echo "bundle exec rake db:schema:load db:seed cortex:create_categories"
-bundle exec rake db:schema:load db:seed cortex:create_categories
-
-echo "bundle exec rake bower:install:deployment"
-bundle exec rake bower:install:deployment
+echo "bundle exec rake cortex:create_categories cortex:onet:fetch_and_provision cortex:core:db:reseed"
+bundle exec rake cortex:create_categories cortex:onet:fetch_and_provision cortex:core:db:reseed
 
 echo "bundle exec rake cortex:rebuild_indexes"
 bundle exec rake cortex:rebuild_indexes

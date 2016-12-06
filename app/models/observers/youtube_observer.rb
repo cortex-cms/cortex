2a80e8679af6a4b2a5bc48f582cbc9723a9a58e9
Alex Tharp <toastercup@gmail.com>
2016-12-06 17:38:49 -0600

    Switch to after_commit hook to resolve YoutubeMediaJob race condition

--- app/models/observers/youtube_observer.rb
+++ app/models/observers/youtube_observer.rb
@@ -1,8 +1,5 @@
 class YoutubeObserver < ActiveRecord::Observer
-  def after_create(media)
-    # Delay job by 5 seconds, as there appears to be a bug somewhere as it relates to STI+Observers+ActiveJob.
-    # The Media object mysteriously will not exist after_create..
-    # TODO: Remove this workaround.
-    YoutubeMediaJob.set(wait: 5.seconds).perform_later(media)
+  def after_commit(media)
+    YoutubeMediaJob.perform_later(media)
   end
 end

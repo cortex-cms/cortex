acbc2bd57d4279cb1f2ac47e2bf14c3b6b83c716
Alex Tharp <toastercup@gmail.com>
2017-01-04 16:39:35 -0600

    authentication.js should use jquery3, little misc changes

--- app/assets/javascripts/authentication.js
+++ app/assets/javascripts/authentication.js
@@ -1,6 +1,9 @@
-//= require jquery
+//= require jquery3
+// require jquery.turbolinks # temporarily disabled
 //= require jquery_ujs
 //= require underscore
 //= require material
 
 //= require base
+
+// require turbolinks # temporarily disabled
--- app/assets/javascripts/datepicker_init.js
+++ app/assets/javascripts/datepicker_init.js
@@ -1,23 +1,25 @@
 var datepicker_init = function() {
   var datepicker = $(".datepicker");
 
-  datepicker.datetimepicker({
-    dateFormat: "dd/mm/yy",
-    onSelect: function (date) {
-      var button = $(".new_publish_state_button");
+  if (datepicker.length) {
+    datepicker.datetimepicker({
+      dateFormat: "dd/mm/yy",
+      onSelect: function (date) {
+        var button = $(".new_publish_state_button");
 
-      if (button.length > 0) {
-        button.val("schedule");
-        button.text("Schedule Post");
+        if (button.length > 0) {
+          button.val("schedule");
+          button.text("Schedule Post");
+        }
       }
-    }
-  });
+    });
 
-  datepicker.on("focusout", function(ev){
-    if ($(this).val() === "") {
-      $(this).closest('div').addClass('is-dirty');
-    }
-  });
+    datepicker.on("focusout", function(ev){
+      if ($(this).val() === "") {
+        $(this).closest('div').addClass('is-dirty');
+      }
+    });
+  }
 };
 
 $(document).ready(datepicker_init);

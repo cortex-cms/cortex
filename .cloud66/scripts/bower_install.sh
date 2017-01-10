9c05f4e317d701c54c62e42e8fc0f363a2538739
Alex Tharp <toastercup@gmail.com>
2017-01-10 14:47:55 -0600

    Manually specify bower:before_precompile as dependency of react_on_rails:assets:compile_environment so we can eliminate C66 bower:install script

--- .cloud66/scripts/bower_install.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-unset BUNDLE_GEMFILE
-cd $STACK_BASE/releases/$(ls -1 -t $STACK_BASE/releases/ | head -n1)
-bundle exec rake bower:install
---
+++ lib/tasks/dependencies.rake
@@ -0,0 +1,1 @@
+task 'react_on_rails:assets:compile_environment' => 'bower:before_precompile'

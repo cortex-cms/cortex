e8f2b563d9834ddbed57eb6c1f0a827fb5b1da2d
Alex Tharp <toastercup@gmail.com>
2017-02-26 13:44:30 -0600

    chore: add some debugging to C66 hooks

--- .cloud66/scripts/clobber_assets.sh
+++ .cloud66/scripts/clobber_assets.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 cd $STACK_BASE/releases/$(ls -1 -t $STACK_BASE/releases/ | head -n1)
+
+echo "Executing clobber_assets in:"
+pwd
+
 rm -rf public/assets/* app/assets/webpack/*
--- .cloud66/scripts/write_plugins_to_tmp.sh
+++ .cloud66/scripts/write_plugins_to_tmp.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 cd $STACK_BASE/releases/$(ls -1 -t $STACK_BASE/releases/ | head -n1)
+
+echo "Executing write_plugins_to_tmp in:"
+pwd
+
 bundle exec rake cortex:plugins:write_to_tmp

9a0d7a852515ecdc96b66048010ee416e30c9b50
Alex Tharp <toastercup@gmail.com>
2017-02-26 17:04:42 -0600

    fix: manually specify Gemfile for write_plugins_to_tmp C66 hook

--- .cloud66/scripts/write_plugins_to_tmp.sh
+++ .cloud66/scripts/write_plugins_to_tmp.sh
@@ -4,4 +4,4 @@ cd $STACK_BASE/releases/$(ls -1 -t $STACK_BASE/releases/ | head -n1)
 echo "Executing write_plugins_to_tmp in:"
 pwd
 
-bundle exec rake cortex:plugins:write_to_tmp
+env BUNDLE_GEMFILE=$STACK_BASE/releases/$(ls -1 -t $STACK_BASE/releases/ | head -n1)/Gemfile bundle exec rake cortex:plugins:write_to_tmp

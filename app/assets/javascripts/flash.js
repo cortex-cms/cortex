4afa1ca572e9c5c692ad7d835713d668919020e1
Peter Halloran <peter.halloran@careerbuilder.com>
2017-03-08 11:30:15 -0600

    fix: remove extra whitespace from flash.js

--- app/assets/javascripts/flash.js
+++ app/assets/javascripts/flash.js
@@ -1,10 +1,6 @@
-
-
-
 setTimeout(function(){
   $('.mdl-js-snackbar').toggleClass('mdl-snackbar--active');
 }, 2000);
-
 
 $('.mdl-snackbar__close').click(function(){
   $('.mdl-snackbar').removeClass('mdl-snackbar--active');

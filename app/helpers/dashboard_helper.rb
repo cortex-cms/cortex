16e3e803c267630ebd330d1cdefc28d85f00e1bc
Alex Tharp <toastercup@gmail.com>
2017-03-20 01:34:00 -0500

    feat: implement jumbo buttons and dashboard quick links, wrap up dashboard notes implementation, setup main content padding

--- app/helpers/dashboard_helper.rb
+++ app/helpers/dashboard_helper.rb
@@ -18,4 +18,12 @@ module DashboardHelper
   def news_feed_tenant
     @news_feed_tenant ||= Tenant.find_by_id(Cortex.config.cortex.news_feed.tenant)
   end
+
+  def media_content_type
+    @media_content_type = ContentType.find_by_name('Media')
+  end
+
+  def employer_blog_content_type
+    @employer_blog_content_type = ContentType.find_by_name('Employer Blog')
+  end
 end

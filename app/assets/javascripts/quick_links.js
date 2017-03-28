f4e9451f9cc504ca636b3b3807567d259a02bef5
Alex Tharp <toastercup@gmail.com>
2017-03-28 15:58:58 -0500

    fix: switch from mousedown to hover for quick links icon toggle, tighten up Notes styling

--- app/assets/javascripts/quick_links.js
+++ app/assets/javascripts/quick_links.js
@@ -2,10 +2,10 @@
   'use strict';
 
   $(document).ready(function() {
-    $('.quick-links .jumbo-button').mousedown(function () {
+    $('.quick-links .jumbo-button').hover(function () {
       $(this).find('.jumbo-button__content__icon').css('display', 'none');
       $(this).find('.jumbo-button__content__add').css('display', 'block');
-    }).bind('mouseup mouseleave', function () {
+    }, function () {
       $(this).find('.jumbo-button__content__icon').css('display', 'block');
       $(this).find('.jumbo-button__content__add').css('display', 'none');
     });

ae224f0604896b43379c3bbd18d4d28a85ce8d64
ElliottAYoung <elliott.a.young@gmail.com>
2017-04-24 14:37:22 -0500

    feat: handle timezone through DatePicker

--- app/assets/javascripts/datepicker_init.js
+++ app/assets/javascripts/datepicker_init.js
@@ -4,20 +4,7 @@ var datepicker_init = function() {
   if (datepicker.length) {
     datepicker.datetimepicker({
       dateFormat: 'dd/mm/yy',
-      onSelect: function (date) {
-        var button = $('.new_publish_state_button');
-        var currentDate = new Date();
-        var selectedDate = moment(date, 'DD-MM-YYYY HH:mm');
-
-        if (button.length > 0 && selectedDate > currentDate) {
-          button.val('schedule');
-          button.text('Schedule Post');
-        }
-        else if (button.length > 0 && selectedDate <= currentDate) {
-          button.val('published');
-          button.text('Publish Now');
-        }
-      }
+      timeFormat: 'HH:mm Z'
     });
 
     datepicker.on('focusout', function(ev){

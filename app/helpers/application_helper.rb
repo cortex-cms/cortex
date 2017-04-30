d04281dcabfe70a8b0cc7475931ffab2cbbf0309
Alex Tharp <toastercup@gmail.com>
2017-04-30 13:57:26 -0500

    feat: environment indicator/banner, missing branding text fix

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,10 +10,27 @@ module ApplicationHelper
   end
 
   def qualtrics_domain
-  	extra_config.qualtrics_id.delete('_').downcase
+    extra_config.qualtrics_id.delete('_').downcase
   end
 
   def flag_enabled?(flag_name)
     Cortex.flipper[flag_name].enabled?(current_user, request)
+  end
+
+  def environment
+    request.local? ? :local : Rails.env
+  end
+
+  def environment_abbreviated
+    case environment
+      when 'production'
+        :prd
+      when 'staging'
+        :stg
+      when 'development'
+        :dev
+      else
+        :loc
+    end
   end
 end

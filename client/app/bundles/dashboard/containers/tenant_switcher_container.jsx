eb34ded44abbebcf8b6903e9774260818949cd73
Peter Halloran <peter.halloran@careerbuilder.com>
2017-09-05 14:37:40 -0500

    feat(containers/tenant_switcher_container.jsx): set TenantSwitcherContainer to remove sidebar--tenant-display class from #layout_wrapper on successful tenant switch

--- client/app/bundles/dashboard/containers/tenant_switcher_container.jsx
+++ client/app/bundles/dashboard/containers/tenant_switcher_container.jsx
@@ -1,22 +1,14 @@
 import React from 'react';
-import axios from 'axios';
-import { NOT_DEFINED } from 'constants/type_constants'
-import {
-  TOGGLE_TENANT_SWITCHER,
-  TOGGLE_SIDEBAR,
-  TENANT_UPDATED,
-  TENANT_UPDATE_ERROR,
-  SELECT_TENANT
-} from 'constants/tenant_switcher'
-import {
-  capitalize
-} from 'dashboard/helpers/formating'
+import SetRailsAPIService from 'dashboard/services/rails_api_service'
+import {NOT_DEFINED} from 'constants/type_constants'
+import {TOGGLE_TENANT_SWITCHER, TOGGLE_SIDEBAR, TENANT_UPDATED, TENANT_UPDATE_ERROR, SELECT_TENANT} from 'constants/tenant_switcher'
+import {capitalize} from 'dashboard/helpers/formating'
 
 import EnvironmentFlag from 'components/side_bar/environment_flag'
 import TenantList from 'components/side_bar/tenant_list'
 
 const getLayoutWrapper = (serverSide) => {
-  if(serverSide === true) {
+  if (serverSide === true) {
     return {}
   }
   return document.getElementById('layout_wrapper')
@@ -26,51 +18,57 @@ class TenantSwitcherContainer extends React.PureComponent {
   constructor(props) {
     super(props);
     this.layoutWrapper = getLayoutWrapper(this.props.railsContext.serverSide)
-    axios.defaults.headers.common['X-CSRF-Token'] = this.props.csrf_token
-    axios.defaults.headers.common['Accept'] = 'application/json'
+    this.railsAPI = SetRailsAPIService(this.props.railsContext, this.props.data)
   }
   selectTenant = (tenant) => {
     console.log('selectTenant tenant', tenant)
     this.updateTenant(tenant)
-    this.props.dispatch({ type: SELECT_TENANT, payload: tenant })
+    this.props.dispatch({type: SELECT_TENANT, payload: tenant})
   }
   updateTenant = (tenant) => {
-    const { current_user, csrf_token } = this.props.data
+    const {current_user, csrf_token} = this.props.data
     const self = this;
-    axios.post(window.location.origin + '/admin_update/tenant_change',{
-            current_tenant: current_user.active_tenant.id,
-            requested_tenant: tenant.id,
-            authenticity_token: csrf_token
-        }).then(response => {
-          console.log('response', response)
-          self.props.dispatch({ type: TENANT_UPDATED, payload: Object.assign({}, current_user, {active_tenant: response.data }) })
+    this.railsAPI.post('/admin_update/tenant_change', {
+      current_tenant: current_user.active_tenant.id,
+      requested_tenant: tenant.id,
+      authenticity_token: csrf_token
+    }).then(response => {
+      self.props.dispatch({type: TENANT_UPDATED, payload: Object.assign({}, current_user, {active_tenant: response.data})})
+      self.layoutWrapper.classList.remove('sidebar--tentant-display')
 
-        }).catch(error => {
-            console.log('error', error)
-            self.props.dispatch({ type: TENANT_UPDATE_ERROR, payload: error })
-        })
+    }).catch(error => {
+      console.log('error', error)
+      self.props.dispatch({type: TENANT_UPDATE_ERROR, payload: error})
+    })
 
   }
   toggleTenantSwitcher = () => {
-      this.props.dispatch({ type: TOGGLE_TENANT_SWITCHER })
-      this.layoutWrapper.className = !this.props.data.tenantListActive ? 'sidebar--tentant-display' : '';
+    this.props.dispatch({type: TOGGLE_TENANT_SWITCHER})
+    this.layoutWrapper.className = !this.props.data.tenantListActive ? 'sidebar--tentant-display' : '';
   }
   toggleSidebar = () => {
-    this.props.dispatch({ type: TOGGLE_SIDEBAR })
-    this.layoutWrapper.className = this.props.data.sidebarExpanded ?  'sidebar--collapsed' : '';
+    this.props.dispatch({type: TOGGLE_SIDEBAR})
+    this.layoutWrapper.className = this.props.data.sidebarExpanded ? 'sidebar--collapsed' : '';
   }
   render() {
-    console.log('this.props', this.props)
-    const { environment, environment_abbreviated, current_user, tenant, selected_tenant, tenants, tenantListActive } = this.props.data
+    const {
+      environment,
+      environment_abbreviated,
+      current_user,
+      tenant,
+      selected_tenant,
+      tenants,
+      tenantListActive
+    } = this.props.data
     const syncedWithDB = current_user.active_tenant.id === selected_tenant.id
     return (
-      <footer id="tentant_switch" >
-        <TenantList selectTenant={this.selectTenant} syncedWithDB={syncedWithDB} current_user={current_user} selected_tenant={selected_tenant} tenants={tenants} active={tenantListActive} />
+      <footer id="tentant_switch">
+        <TenantList selectTenant={this.selectTenant} syncedWithDB={syncedWithDB} current_user={current_user} selected_tenant={selected_tenant} tenants={tenants} active={tenantListActive}/>
         <nav className='demo-navigation mdl-navigation'>
-          <EnvironmentFlag environment={environment} environment_abbreviated={environment_abbreviated} />
+          <EnvironmentFlag environment={environment} environment_abbreviated={environment_abbreviated}/>
           <div onClick={this.toggleTenantSwitcher} className='mdl-navigation__link nav__item'>
             <span className='nav__item-name'>
-              { syncedWithDB === true ? selected_tenant.name : 'Loading...'}
+              {syncedWithDB === true ? selected_tenant.name : 'Loading...'}
             </span>
             <i className='material-icons' role="presentation">device_hub</i>
           </div>

d33ff8e670320c763bd9ad59b77fb7f8f53d3231
Peter Halloran <peter.halloran@careerbuilder.com>
2017-09-11 13:13:46 -0500

    feat: create Wizard conatiner

---
+++ client/app/bundles/cortex/containers/wizard_container.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { NOT_DEFINED } from 'constants/type_constants'
+
+
+class WizardContainer extends React.PureComponent {
+  render() {
+
+  }
+}
+
+export default WizardContainer

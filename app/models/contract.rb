0b5a57966b885f67b623605c73a0cd8cd949d1cb
Alex Tharp <toastercup@gmail.com>
2017-09-22 11:53:43 -0500

    docs(Contract): add refactor note

--- app/models/contract.rb
+++ app/models/contract.rb
@@ -1,4 +1,5 @@
 class Contract < ApplicationRecord
+  # TODO: Refactor
   has_many :content_types
   has_many :contentable_decorators, as: :contentable
   has_many :decorators, through: :contentable_decorators

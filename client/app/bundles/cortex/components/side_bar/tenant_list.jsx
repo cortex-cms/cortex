02daa0d657172d8295fe64cfa85d7a5ec61c9317
Peter Halloran <peter.halloran@careerbuilder.com>
2017-09-28 12:09:46 -0500

    feat(helpers/tenant_ancestry.jsx): create tenant ancestry helper method

--- client/app/bundles/cortex/components/side_bar/tenant_list.jsx
+++ client/app/bundles/cortex/components/side_bar/tenant_list.jsx
@@ -11,7 +11,8 @@ import List, {
 } from 'material-ui/List';
 import Avatar from 'material-ui/Avatar';
 import IconButton from 'material-ui/IconButton';
-import { NOT_DEFINED } from 'constants/type_constants'
+import { NOT_DEFINED } from 'constants/type_constants';
+import TenantAncestryList from  'cortex/helpers/tenant_ancestry';
 
 const activeListItemStyles = {
   root: {
@@ -50,13 +51,12 @@ class TenantList extends React.PureComponent {
     return listedTenantIds.map((tenant_id, index) =>   TenantItem(tenancyLookup[tenant_id], selectTenant(tenancyLookup[tenant_id]), activeTenant === tenancyLookup[tenant_id].id, subTenantListClicked(tenancyLookup[tenant_id].id)) )
   }
   tenantParentHeader = ({parent_tenant, tenancyLookup, paginateBack}) => {
-    let parentTenant = parent_tenant === null ? '' : tenancyLookup[parent_tenant].name;
     return (
       <div className={ parent_tenant === null ? 'hidden' : 'tenant__list-nav-header'}>
          <IconButton aria-label="Go Back" className='page-back-button' onClick={paginateBack}>
           <i className='material-icons'>chevron_left</i>
          </IconButton>
-        <span>{parentTenant}</span>
+        <span>{TenantAncestryList(parent_tenant, tenancyLookup).join(' < ')}</span>
       </div>
     )
   }

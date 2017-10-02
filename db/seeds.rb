515eabc11e994e3c44f76940311540cbd68af13c
Alex Tharp <toastercup@gmail.com>
2017-10-01 19:52:58 -0500

    feat(Seeds): example tenant + multiple tenants for initial user

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,17 +1,26 @@
 # Create root tenant and user
-tenant_seed = SeedData.cortex_tenant
-user_seed = tenant_seed.creator
+cortex_tenant_seed = SeedData.cortex_tenant
+example_tenant_seed = SeedData.example_tenant
+user_seed = cortex_tenant_seed.creator
 
-existing_tenant = Tenant.find_by_name(tenant_seed.name)
+existing_tenant = Tenant.find_by_name(cortex_tenant_seed.name)
 
 unless existing_tenant
-  cortex_tenant = Tenant.new(name: tenant_seed.name, description: tenant_seed.description)
-
-  cortex_tenant.owner = User.new(email: user_seed.email,
-                                 firstname: user_seed.firstname,
-                                 lastname: user_seed.lastname,
-                                 password: user_seed.password,
-                                 password_confirmation: user_seed.password)
+  user = User.new(email: user_seed.email,
+                  firstname: user_seed.firstname,
+                  lastname: user_seed.lastname,
+                  password: user_seed.password,
+                  password_confirmation: user_seed.password)
+  cortex_tenant = Tenant.new(name: cortex_tenant_seed.name,
+                             description: cortex_tenant_seed.description,
+                             owner: user)
+  example_tenant = Tenant.new(name: example_tenant_seed.name,
+                              description: example_tenant_seed.description,
+                              owner: user)
 
   cortex_tenant.save!
+  example_tenant.save!
+
+  user.tenants = [cortex_tenant, example_tenant]
+  user.save!
 end

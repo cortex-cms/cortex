432a7ae08c3363f1df3c3a71956c41be88f653ac
Alex Tharp <toastercup@gmail.com>
2017-11-11 21:17:54 -0500

    refactor(ElasticSearch): migrate to Elasticsearch::DSL query builder

--- app/models/concerns/searchable.rb
+++ app/models/concerns/searchable.rb
@@ -5,32 +5,5 @@ module Searchable
     include Elasticsearch::Model
 
     index_name "#{Rails.env}_#{model_name.to_s.parameterize(separator: '_')}"
-
-    class << self
-      def query_massage q
-        return '*' if q.nil? or q.empty?
-        result = q.chomp.strip
-        return '*' if result.nil? or result.empty?
-        result.index(/\s|\-/).nil? ? "#{result}*" : result
-      end
-
-      def term_search(field, q)
-        result = {
-          term: { field => q }
-        }
-      end
-
-      def terms_search(field, q)
-        result = {
-          terms: { field => q }
-        }
-      end
-
-      def range_search(field, type, q)
-        result = {
-          range: { field => { type => q } }
-        }
-      end
-    end
   end
 end
--- app/transactions/get_content_items_for_content_type_transaction.rb
+++ app/transactions/get_content_items_for_content_type_transaction.rb
@@ -1,14 +1,29 @@
 class GetContentItemsForContentTypeTransaction < ApplicationTransaction
+  include Elasticsearch::DSL
+
   step :search
 
   def search(params)
     tenant_id = params[:args][:tenant_id] || params[:active_tenant].id
+    content_type_id = params[:content_type].id
 
-    tenant_filter = { term: { tenant_id: tenant_id } }
-    content_type_filter = { term: { content_type_id: params[:content_type].id } }
-    bool = { bool: { filter: [tenant_filter, content_type_filter] } }
+    definition = Elasticsearch::DSL::Search.search {
+      query do
+        bool do
+          filter do
+            term tenant_id: tenant_id
+          end
+          filter do
+            term content_type_id: content_type_id
+          end
+        end
+      end
+      sort created_at: {
+        order: 'desc'
+      }
+    }
 
-    results = ContentItem.search({ query: bool, sort: [{ created_at: { order: 'desc' } }] }, index: params[:content_type].content_items_index_name)
+    results = ContentItem.search(definition, index: params[:content_type].content_items_index_name)
     # TODO: basic pagination
     Right(results.records)
   end

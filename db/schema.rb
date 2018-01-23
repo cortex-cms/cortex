73bb43b93b2972e3429c8a11d9cae0db33e0c667
Peter Halloran <peter.halloran@careerbuilder.com>
2018-01-23 10:28:08 -0600

    fix(db/schema.rb): rebuilt schema for legacy-develop

--- db/schema.rb
+++ db/schema.rb
@@ -17,7 +17,6 @@ ActiveRecord::Schema.define(version: 20180117221543) do
   enable_extension "hstore"
   enable_extension "uuid-ossp"
   enable_extension "citext"
-  enable_extension "pgcrypto"
 
   create_table "applications", force: :cascade do |t|
     t.string   "name"
@@ -84,78 +83,59 @@ ActiveRecord::Schema.define(version: 20180117221543) do
     t.integer "category_id", null: false
   end
 
-  create_table "content_items", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
-    t.string   "state"
-    t.uuid     "content_type_id", null: false
-    t.uuid     "tenant_id",       null: false
-    t.uuid     "creator_id",      null: false
-    t.uuid     "updated_by_id"
+  create_table "content_items", id: :uuid, default: -> { "uuid_generate_v4()" }, force: :cascade do |t|
+    t.uuid     "content_type_id"
+    t.datetime "created_at",                      null: false
+    t.datetime "updated_at",                      null: false
     t.datetime "deleted_at"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
-    t.index ["content_type_id"], name: "index_content_items_on_content_type_id", using: :btree
-    t.index ["creator_id"], name: "index_content_items_on_creator_id", using: :btree
+    t.boolean  "is_published",    default: false
+    t.integer  "updated_by_id"
+    t.string   "state"
+    t.datetime "publish_date"
+    t.integer  "creator_id",                      null: false
     t.index ["deleted_at"], name: "index_content_items_on_deleted_at", using: :btree
-    t.index ["state"], name: "index_content_items_on_state", using: :btree
-    t.index ["tenant_id"], name: "index_content_items_on_tenant_id", using: :btree
-    t.index ["updated_by_id"], name: "index_content_items_on_updated_by_id", using: :btree
+    t.index ["id"], name: "index_content_items_on_id", using: :btree
   end
 
-  create_table "content_types", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
-    t.string   "name",                         null: false
-    t.string   "name_id",                      null: false
+  create_table "content_types", id: :uuid, default: -> { "uuid_generate_v4()" }, force: :cascade do |t|
+    t.string   "name"
     t.text     "description"
-    t.uuid     "creator_id",                   null: false
-    t.uuid     "tenant_id",                    null: false
-    t.uuid     "contract_id",                  null: false
-    t.boolean  "publishable", default: false,  null: false
-    t.string   "icon",        default: "help"
-    t.datetime "deleted_at"
+    t.integer  "creator_id",                   null: false
     t.datetime "created_at",                   null: false
     t.datetime "updated_at",                   null: false
-    t.index ["contract_id"], name: "index_content_types_on_contract_id", using: :btree
-    t.index ["creator_id"], name: "index_content_types_on_creator_id", using: :btree
+    t.datetime "deleted_at"
+    t.uuid     "contract_id"
+    t.string   "icon",        default: "help", null: false
+    t.boolean  "publishable", default: false
     t.index ["deleted_at"], name: "index_content_types_on_deleted_at", using: :btree
-    t.index ["name"], name: "index_content_types_on_name", using: :btree
-    t.index ["name_id"], name: "index_content_types_on_name_id", using: :btree
-    t.index ["tenant_id"], name: "index_content_types_on_tenant_id", using: :btree
+    t.index ["id"], name: "index_content_types_on_id", using: :btree
   end
 
-  create_table "contentable_decorators", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+  create_table "contentable_decorators", id: :uuid, default: -> { "uuid_generate_v4()" }, force: :cascade do |t|
     t.uuid     "decorator_id"
-    t.string   "contentable_type"
     t.uuid     "contentable_id"
-    t.uuid     "tenant_id",        null: false
-    t.datetime "deleted_at"
+    t.string   "contentable_type"
     t.datetime "created_at",       null: false
     t.datetime "updated_at",       null: false
-    t.index ["contentable_type", "contentable_id"], name: "index_contentable_decorators_on_contentable", using: :btree
+    t.index ["contentable_id"], name: "index_contentable_decorators_on_contentable_id", using: :btree
+    t.index ["contentable_type"], name: "index_contentable_decorators_on_contentable_type", using: :btree
     t.index ["decorator_id"], name: "index_contentable_decorators_on_decorator_id", using: :btree
-    t.index ["deleted_at"], name: "index_contentable_decorators_on_deleted_at", using: :btree
-    t.index ["tenant_id"], name: "index_contentable_decorators_on_tenant_id", using: :btree
+    t.index ["id"], name: "index_contentable_decorators_on_id", using: :btree
   end
 
-  create_table "contracts", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
-    t.string   "name",       null: false
-    t.uuid     "tenant_id",  null: false
-    t.datetime "deleted_at"
+  create_table "contracts", id: :uuid, default: -> { "uuid_generate_v4()" }, force: :cascade do |t|
+    t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["deleted_at"], name: "index_contracts_on_deleted_at", using: :btree
-    t.index ["name"], name: "index_contracts_on_name", using: :btree
-    t.index ["tenant_id"], name: "index_contracts_on_tenant_id", using: :btree
+    t.index ["id"], name: "index_contracts_on_id", using: :btree
   end
 
-  create_table "decorators", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
-    t.string   "name",                    null: false
-    t.jsonb    "data",       default: {}, null: false
-    t.uuid     "tenant_id",               null: false
-    t.datetime "deleted_at"
-    t.datetime "created_at",              null: false
-    t.datetime "updated_at",              null: false
-    t.index ["deleted_at"], name: "index_decorators_on_deleted_at", using: :btree
-    t.index ["name"], name: "index_decorators_on_name", using: :btree
-    t.index ["tenant_id"], name: "index_decorators_on_tenant_id", using: :btree
+  create_table "decorators", id: :uuid, default: -> { "uuid_generate_v4()" }, force: :cascade do |t|
+    t.string   "name"
+    t.jsonb    "data"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["id"], name: "index_decorators_on_id", using: :btree
   end
 
   create_table "documents", force: :cascade do |t|
@@ -168,55 +148,52 @@ ActiveRecord::Schema.define(version: 20180117221543) do
     t.index ["user_id"], name: "index_documents_on_user_id", using: :btree
   end
 
-  create_table "field_items", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
-    t.jsonb    "data",            default: {}, null: false
-    t.uuid     "field_id",                     null: false
-    t.uuid     "content_item_id",              null: false
-    t.datetime "deleted_at"
+  create_table "field_items", id: :uuid, default: -> { "uuid_generate_v4()" }, force: :cascade do |t|
+    t.uuid     "field_id"
+    t.uuid     "content_item_id"
     t.datetime "created_at",                   null: false
     t.datetime "updated_at",                   null: false
+    t.datetime "deleted_at"
+    t.jsonb    "data",            default: {}
     t.index ["content_item_id"], name: "index_field_items_on_content_item_id", using: :btree
-    t.index ["data"], name: "index_field_items_on_data", using: :gin
     t.index ["deleted_at"], name: "index_field_items_on_deleted_at", using: :btree
     t.index ["field_id"], name: "index_field_items_on_field_id", using: :btree
+    t.index ["id"], name: "index_field_items_on_id", using: :btree
   end
 
-  create_table "field_types", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+  create_table "field_types", id: :uuid, default: -> { "uuid_generate_v4()" }, force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table "fields", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
-    t.string   "name",                         null: false
-    t.string   "name_id",                      null: false
-    t.string   "field_type",                   null: false
-    t.jsonb    "metadata",        default: {}, null: false
-    t.jsonb    "validations",     default: {}, null: false
-    t.uuid     "content_type_id",              null: false
+  create_table "fields", id: :uuid, default: -> { "uuid_generate_v4()" }, force: :cascade do |t|
+    t.uuid     "content_type_id",                           null: false
+    t.string   "field_type",                                null: false
+    t.boolean  "required",        default: false,           null: false
+    t.datetime "created_at",                                null: false
+    t.datetime "updated_at",                                null: false
+    t.jsonb    "validations",     default: {}
     t.datetime "deleted_at"
-    t.datetime "created_at",                   null: false
-    t.datetime "updated_at",                   null: false
-    t.index ["content_type_id"], name: "index_fields_on_content_type_id", using: :btree
+    t.string   "name"
+    t.jsonb    "metadata",        default: {"default"=>{}}
     t.index ["deleted_at"], name: "index_fields_on_deleted_at", using: :btree
-    t.index ["field_type"], name: "index_fields_on_field_type", using: :btree
-    t.index ["name"], name: "index_fields_on_name", using: :btree
-    t.index ["name_id"], name: "index_fields_on_name_id", using: :btree
+    t.index ["id"], name: "index_fields_on_id", using: :btree
   end
 
-  create_table "flipper_features", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+  create_table "flipper_features", force: :cascade do |t|
     t.string   "key",        null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["key"], name: "index_flipper_features_on_key", unique: true, using: :btree
   end
 
-  create_table "flipper_gates", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+  create_table "flipper_gates", force: :cascade do |t|
     t.string   "feature_key", null: false
     t.string   "key",         null: false
     t.string   "value"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
-    t.index ["feature_key"], name: "index_flipper_gates_on_feature_key", unique: true, using: :btree
-    t.index ["key"], name: "index_flipper_gates_on_key", unique: true, using: :btree
-    t.index ["value"], name: "index_flipper_gates_on_value", unique: true, using: :btree
+    t.index ["feature_key", "key", "value"], name: "index_flipper_gates_on_feature_key_and_key_and_value", unique: true, using: :btree
   end
 
   create_table "locales", id: :uuid, default: -> { "uuid_generate_v4()" }, force: :cascade do |t|
@@ -319,24 +296,12 @@ ActiveRecord::Schema.define(version: 20180117221543) do
     t.integer "onet_occupation_id", null: false
   end
 
-  create_table "permissions", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+  create_table "permissions", force: :cascade do |t|
     t.string   "name"
     t.string   "resource_type"
-    t.uuid     "resource_id"
-    t.datetime "deleted_at"
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
-    t.index ["deleted_at"], name: "index_permissions_on_deleted_at", using: :btree
-    t.index ["name"], name: "index_permissions_on_name", using: :btree
-    t.index ["resource_id"], name: "index_permissions_on_resource_id", using: :btree
-    t.index ["resource_type"], name: "index_permissions_on_resource_type", using: :btree
-  end
-
-  create_table "permissions_roles", id: false, force: :cascade do |t|
-    t.uuid "permission_id", null: false
-    t.uuid "role_id",       null: false
-    t.index ["permission_id"], name: "index_permissions_roles_on_permission_id", using: :btree
-    t.index ["role_id"], name: "index_permissions_roles_on_role_id", using: :btree
+    t.integer  "resource_id"
   end
 
   create_table "posts", force: :cascade do |t|
@@ -389,23 +354,13 @@ ActiveRecord::Schema.define(version: 20180117221543) do
     t.datetime "updated_at",    null: false
   end
 
-  create_table "roles", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+  create_table "roles", force: :cascade do |t|
     t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.string   "resource_type"
-    t.uuid     "resource_id"
-    t.datetime "deleted_at"
-    t.datetime "created_at",    null: false
-    t.datetime "updated_at",    null: false
-    t.index ["deleted_at"], name: "index_roles_on_deleted_at", using: :btree
+    t.string   "resource_id"
     t.index ["name"], name: "index_roles_on_name", using: :btree
-    t.index ["resource_type", "resource_id"], name: "index_roles_on_resource_type_and_resource_id", using: :btree
-  end
-
-  create_table "roles_users", id: false, force: :cascade do |t|
-    t.uuid "role_id", null: false
-    t.uuid "user_id", null: false
-    t.index ["role_id"], name: "index_roles_users_on_role_id", using: :btree
-    t.index ["user_id"], name: "index_roles_users_on_user_id", using: :btree
   end
 
   create_table "snippets", force: :cascade do |t|
@@ -444,57 +399,49 @@ ActiveRecord::Schema.define(version: 20180117221543) do
     t.index ["name"], name: "index_tags_on_name", unique: true, using: :btree
   end
 
-  create_table "tenants", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
-    t.string   "name",        limit: 50, null: false
-    t.string   "name_id",                null: false
-    t.text     "description"
-    t.uuid     "parent_id"
+  create_table "tenants", force: :cascade do |t|
+    t.string   "name",          limit: 50,  null: false
+    t.string   "subdomain",     limit: 50
+    t.integer  "parent_id"
     t.integer  "lft"
     t.integer  "rgt"
     t.integer  "depth"
+    t.string   "contact_name",  limit: 50
+    t.string   "contact_email", limit: 200
+    t.string   "contact_phone", limit: 20
     t.datetime "deleted_at"
+    t.string   "contract",      limit: 255
+    t.string   "did",           limit: 255
     t.datetime "active_at"
     t.datetime "deactive_at"
-    t.uuid     "owner_id"
-    t.datetime "created_at",             null: false
-    t.datetime "updated_at",             null: false
-    t.index ["deleted_at"], name: "index_tenants_on_deleted_at", using: :btree
-    t.index ["name"], name: "index_tenants_on_name", unique: true, using: :btree
-    t.index ["name_id"], name: "index_tenants_on_name_id", unique: true, using: :btree
-    t.index ["owner_id"], name: "index_tenants_on_owner_id", using: :btree
+    t.integer  "owner_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.index ["parent_id"], name: "index_tenants_on_parent_id", using: :btree
   end
 
-  create_table "tenants_users", id: false, force: :cascade do |t|
-    t.uuid "tenant_id", null: false
-    t.uuid "user_id",   null: false
-    t.index ["tenant_id"], name: "index_tenants_users_on_tenant_id", using: :btree
-    t.index ["user_id"], name: "index_tenants_users_on_user_id", using: :btree
-  end
-
-  create_table "users", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
-    t.string   "email",                             default: "",      null: false
-    t.string   "firstname",                                           null: false
-    t.string   "lastname",                                            null: false
-    t.string   "locale",                 limit: 30, default: "en_US", null: false
-    t.string   "timezone",               limit: 30, default: "EST",   null: false
-    t.datetime "deleted_at"
-    t.datetime "created_at",                                          null: false
-    t.datetime "updated_at",                                          null: false
-    t.uuid     "active_tenant_id"
-    t.string   "encrypted_password",                default: "",      null: false
-    t.string   "reset_password_token"
+  create_table "users", force: :cascade do |t|
+    t.string   "email",                  limit: 255, default: "",      null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer  "tenant_id",                                            null: false
+    t.string   "encrypted_password",     limit: 255, default: "",      null: false
+    t.string   "reset_password_token",   limit: 255
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",                     default: 0,       null: false
+    t.integer  "sign_in_count",                      default: 0,       null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
-    t.index ["active_tenant_id"], name: "index_users_on_active_tenant_id", using: :btree
-    t.index ["deleted_at"], name: "index_users_on_deleted_at", using: :btree
+    t.string   "current_sign_in_ip",     limit: 255
+    t.string   "last_sign_in_ip",        limit: 255
+    t.string   "firstname",              limit: 255,                   null: false
+    t.string   "lastname",               limit: 255
+    t.string   "locale",                 limit: 30,  default: "en_US", null: false
+    t.string   "timezone",               limit: 30,  default: "EST",   null: false
+    t.boolean  "admin",                              default: false,   null: false
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+    t.index ["tenant_id"], name: "index_users_on_tenant_id", using: :btree
   end
 
   create_table "users_roles", id: false, force: :cascade do |t|
@@ -529,33 +476,11 @@ ActiveRecord::Schema.define(version: 20180117221543) do
     t.jsonb    "buy_box_widget"
     t.jsonb    "carousels_widget"
     t.jsonb    "galleries_widget"
-    t.jsonb    "form_configs"
     t.jsonb    "product_data"
+    t.jsonb    "form_configs"
     t.jsonb    "card_group_widget"
     t.index ["user_id"], name: "index_webpages_on_user_id", using: :btree
   end
 
-  add_foreign_key "content_items", "content_types"
-  add_foreign_key "content_items", "tenants"
-  add_foreign_key "content_items", "users", column: "creator_id"
-  add_foreign_key "content_items", "users", column: "updated_by_id"
-  add_foreign_key "content_types", "contracts"
-  add_foreign_key "content_types", "tenants"
-  add_foreign_key "content_types", "users", column: "creator_id"
-  add_foreign_key "contentable_decorators", "decorators"
-  add_foreign_key "contentable_decorators", "tenants"
-  add_foreign_key "contracts", "tenants"
-  add_foreign_key "decorators", "tenants"
-  add_foreign_key "field_items", "content_items"
-  add_foreign_key "field_items", "fields"
-  add_foreign_key "fields", "content_types"
-  add_foreign_key "permissions_roles", "permissions"
-  add_foreign_key "permissions_roles", "roles"
   add_foreign_key "posts", "carotenes"
-  add_foreign_key "roles_users", "roles"
-  add_foreign_key "roles_users", "users"
-  add_foreign_key "tenants", "users", column: "owner_id"
-  add_foreign_key "tenants_users", "tenants"
-  add_foreign_key "tenants_users", "users"
-  add_foreign_key "users", "tenants", column: "active_tenant_id"
 end

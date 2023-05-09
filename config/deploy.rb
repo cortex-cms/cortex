18343cbb71acc7040bc069ae214f583a7cd36451
Saurabh Sharma <saurabh.sharma@careerbuilder.com>
2023-05-09 16:22:35 +0530

    removed public/assets from shared folders, due to fontawesome issue

--- config/deploy.rb
+++ config/deploy.rb
@@ -55,7 +55,7 @@ after 'deploy:reverting', 'remote:application_setup'
 
 # Default value for linked_dirs is []
 # append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "tmp/webpacker", "public/system", "vendor", "storage"
-append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/assets', 'vendor', 'bower_components'
+append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor', 'bower_components'
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }

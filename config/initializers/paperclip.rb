0162346b4589626db4e08454ae02171f5029177e
Saurabh Sharma <saurabh.sharma@careerbuilder.com>
2023-05-15 17:26:33 +0530

    paperclip fixes

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,13 +1,27 @@
 module Paperclip
   class HashieMashUploadedFileAdapter < AbstractAdapter
+    attr_accessor :original_filename
+
     def initialize(target)
       @tempfile, @content_type, @size = target.tempfile, target.type, target.tempfile.size
       self.original_filename = target.filename
     end
+  end
 
+  class HashUploadedFileAdapter < AbstractAdapter
+    attr_accessor :original_filename
+
+    def initialize(target)
+      @tempfile, @content_type, @size = target[:tempfile], target[:type], target[:tempfile].size
+		self.original_filename = target[:filename]
+    end
   end
 end
 
 Paperclip.io_adapters.register Paperclip::HashieMashUploadedFileAdapter do |target|
   target.is_a? Hashie::Mash
+end
+
+Paperclip.io_adapters.register Paperclip::HashUploadedFileAdapter do |target|
+  target.is_a?(Hash) && target[:tempfile].is_a?(Tempfile)
 end
